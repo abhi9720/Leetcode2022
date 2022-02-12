32be9c3f18af3f8bafa87bed3217de6422efcd9b
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-02-12 21:28:41 +0530

    Time: 0 ms (100.00%), Space: 61.5 MB (69.31%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -1,29 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    public int minDepth(TreeNode root) {
-        if(root==null) return 0;
+  public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
         
-       
-        if(root.left==null) return minDepth(root.right)+1;
-        else if(root.right==null)  return minDepth(root.left)+1;
-        else{ 
-                int left =  minDepth(root.left);
-                int right =  minDepth(root.right);
-                return Math.min(left,right)+1;
+        int depth = 1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode curNode = queue.poll();
+                if(curNode.left == null && curNode.right == null)
+                    return depth;
+                if(curNode.left != null) queue.add(curNode.left);
+                if(curNode.right != null) queue.add(curNode.right);
+            }
+            depth++;
         }
+        return depth;
     }
 }
