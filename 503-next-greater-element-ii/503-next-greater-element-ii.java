2ea1d946aaebf2a8fb972c18cf2e5f09290b58fa
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-02-15 21:12:16 +0530

    Time: 46 ms (19.02%), Space: 55.4 MB (6.61%) - LeetHub

---
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n =  nums.length;
+        Stack<Integer> stk =  new Stack<Integer>();
+        int ans[] =  new int[n];
+        Arrays.fill(ans,-1);
+        int index =  0;
+        int count  = 0;
+        for(int i=0;i<n && count<n; i=(i+1)%n ){
+            // those which are removed by me , for those element i am the nge
+            while(stk.size()>0 && nums[stk.peek()] < nums[i]  ){
+                ans[stk.pop()]  = nums[i];                
+            }            
+            stk.push(i); 
+            if(i==0){
+                count++;
+            }
+            if(count==3){
+                break;
+            }
+        }
+        return ans;        
+    }
+}
