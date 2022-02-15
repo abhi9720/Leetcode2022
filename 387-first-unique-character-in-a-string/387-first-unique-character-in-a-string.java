c7656a7719239b67b70e78a242d40f08fff511c1
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-02-15 16:28:39 +0530

    Time: 10 ms (79.90%), Space: 51.4 MB (32.23%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,31 +1,51 @@
 class Solution {
+    // Approach 1: sliding window technique
+//     public int firstUniqChar(String s) {
+//         int i = -1, j = 0;
+//         HashMap < Character, Integer > freq = new HashMap < > ();
+//         while (j < s.length()) {
+//             if (!freq.containsKey(s.charAt(j))) {
+//                 freq.put(s.charAt(j), 1);
+//             } else {
+
+//                 freq.put(s.charAt(j), freq.getOrDefault(s.charAt(j), 0) + 1);
+
+//                 // means this is no longer first unique
+//                 while (i != -1 && i < j && freq.get(s.charAt(i)) != 1) {
+//                     i++;
+//                 }
+//                 if (i != -1 && freq.get(s.charAt(i)) != 1) {
+//                     i = -1;
+//                 }
+//             }
+
+//             if (i == -1 && freq.get(s.charAt(j)) == 1) {
+//                 i = j;
+//             }
+
+//             j++;
+//         }
+//         return i;
+//     }
+    
     public int firstUniqChar(String s) {
-        int i=-1,j=0;
-        HashMap<Character ,Integer> freq = new HashMap<>();
-        while(j<s.length()){
-            if(!freq.containsKey(s.charAt(j)) ){
-                freq.put(s.charAt(j),1);
+        int freq[] =  new int[26];
+        Arrays.fill(freq,-1);
+        int index =  0;
+        for(char ch : s.toCharArray() ){
+            if(freq[ch-'a']==-1){
+                freq[ch-'a']=index;
             }else{
-                
-                freq.put(s.charAt(j),freq.getOrDefault(s.charAt(j),0)+1);                 
-                
-                 
-                     // means this is no longer first unique
-                     while(i!=-1 && i<j && freq.get(s.charAt(i))!=1 ){                         
-                         i++;
-                     }
-                     if(i!=-1 && freq.get(s.charAt(i))!=1){
-                         i=-1;
-                     }
-                                               
-            }   
-            
-            if(i==-1 && freq.get(s.charAt(j))==1  ){
-                i=j;
+                freq[ch-'a']=-2;
             }
-            
-            j++;
+            index++;
         }
-        return i;                     
+        int MinIndex =  s.length();
+        for(char ch : s.toCharArray() ){
+            if(freq[ch-'a'] >=0 ){
+                MinIndex =  Math.min(MinIndex,freq[ch-'a']);
+            }
+        }
+        return MinIndex==s.length()?-1:MinIndex;                
     }
 }
