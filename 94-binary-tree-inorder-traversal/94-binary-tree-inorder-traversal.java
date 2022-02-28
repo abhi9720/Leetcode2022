99d0598bfe591be914b8c60e8d4168cd3f9f4170
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-02-28 23:29:13 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (7.80%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -14,49 +14,35 @@
  * }
  */
 class Solution {
-    class pair{
-        TreeNode node;
-        int state = -1;
-        pair(TreeNode node){
-            this.node =  node;
-            this.state =  -1;                             
-        }
-        void incr(){
-            this.state =  this.state+1;
-        }
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> inorder =  new ArrayList<>();
-        if(root==null) return inorder;
-        Stack<pair> stk =  new Stack<pair>();
-        stk.add(new pair(root));
-        
-        while(!stk.isEmpty() ){
-            pair peek  =  stk.peek();
-            if(peek.state==-1){
-                // insert left child
-                if(peek.node.left!=null){
-                    stk.add(new pair( peek.node.left ) );
-                }
-                
-                peek.incr();
-            }
-            else if(peek.state==0){
-                // inorder 
-                inorder.add(peek.node.val);
-                peek.incr();
+        List<Integer> ans  =  new ArrayList<>();
+        TreeNode curr =  root;
+        while(curr!=null){
+            TreeNode left  =  curr.left;
+            if(left==null){
+                ans.add(curr.val);
+                curr =  curr.right;
             }
             else{
-                // insert right child 
-                stk.pop();
-                if(peek.node.right!=null){
-                    stk.add(new pair( peek.node.right ) );
+                TreeNode rmn =  giveRightMostNode(left,curr);
+                if(rmn.right==null){// no thread set , so thread creation
+                    rmn.right =  curr;
+                    curr =  curr.left;
+                }else{// if thread alreaady exists , then remove previous thread
+                    rmn.right = null;
+                    ans.add(curr.val);
+                    curr =  curr.right;                    
                 }
             }
+            
         }
-         
-        return inorder;
-        
-        
+        return ans;                        
+    }
+    
+    private TreeNode giveRightMostNode(TreeNode node , TreeNode curr){
+        while(node.right!=null && node.right!=curr){
+            node =  node.right;
+        }
+        return node;
     }
 }
