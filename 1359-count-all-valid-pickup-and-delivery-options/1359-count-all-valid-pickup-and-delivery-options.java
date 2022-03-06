ff693241b19002131ed9f8e5c5c21d79712b479d
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-07 01:20:40 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (41.87%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,20 +1,23 @@
 class Solution {
     public int countOrders(int n) {
-        if(n==1) return 1;
-        else if(n==2) return 6;
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 6;
-        
-        long mod = 1000000007;
-        for(int i=3;i<=n;i++)
+        long res = 1, mod = 1000000007;
+        for(int i = 1; i <=n; ++i)
         {
-            long k = (long) (i*2 -1);    // k = total number of places in n-1
-            long each = (k*(k+1))/2;     //total possibilities in each case of n-1
-            long total = each * (long)dp[i-1];    //total possibilities for n 
-            dp[i] = (int) (total%mod);
+			// calulating n!
+            res *= i;
+			// to avoid overflow
+            res %= mod;
+			
+			// (2*i-1) are the vacant places which we can choose
+			// later we have n! permutation of pickup task
+			// therefore we have to multiply with n! that we calculated as res.
+            res *= (2*i-1);
+			
+			// to avoid overflow
+            res %= mod;
         }
         
-        return dp[n];
+		// to avoid overflow
+        return (int)(res % mod);
     }
 }
