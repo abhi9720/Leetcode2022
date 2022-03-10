f8da491cdafb62f91da95d4828f5db670e24ca7f
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-10 22:26:56 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (64.78%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,34 +1,26 @@
 class Solution {
     public int numDecodings(String s) {
-        if(s.length()==0){
-            return 0;
-        }
-         HashMap<Integer, Integer> map =  new HashMap<>();
-        return helper(s,map);
-         
-    }
-    
-    public int helper(String s ,  HashMap<Integer, Integer> map ){
-        if(s.length()==0){
-            return 1;
-        }
-        
-         int check   = map.getOrDefault(s.length(),-1);
-        if(check!=-1) return check;
-        
-        
-        int count = 0;
-        if(s.charAt(0)!='0'){
-            count+=helper(s.substring(1),map);
-
-        }
-        if(s.length()>=2) {
-            int num  =  (s.charAt(0)-'0')*10+(s.charAt(1)-'0');
-            if(num>=10 && num <=26){
-                count+=  helper(s.substring(2),map);
+        int iminus2  =  0;
+        int iminus1  =  1;
+        int  n =  s.length();
+        for(int i=n-1;i>=0;i-- ){
+            int count  = 0;
+            char ch =  s.charAt(i);
+            if(ch!='0'){
+                count+=iminus1;
+                if(i<s.length() -1){
+                    int num =  (ch-'0')*10+(s.charAt(i+1) - '0') ;
+                    if(num<=26){
+                        count+=iminus2;
+                    }
+                }
             }
+            
+            int ith =  count;
+            iminus2 =  iminus1;
+            iminus1 =  ith;
         }
-         map.put(s.length(),count);
-        return count;
+        return iminus1;
+        
     }
 }
