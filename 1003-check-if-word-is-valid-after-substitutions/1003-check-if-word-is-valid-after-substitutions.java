6cfe21d0c36cc2d1708f2d98001d5377f6240357
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-14 09:52:23 +0530

    Time: 9 ms (90.79%), Space: 46.9 MB (15.74%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -1,24 +1,12 @@
 class Solution {
-    public boolean isValid(String s) {
-        
-        Stack<Character> stk = new Stack<>();
-        
-        for(char ch : s.toCharArray()){            
-            if(ch=='a'){
-                stk.push(ch);
-            }
-            else if(ch=='b'){
-                if(stk.isEmpty()) return false;
-                if(stk.peek()=='a') stk.pop();
-                else return false;
-                stk.push(ch);
-            }
-            else if(ch=='c'){
-                if(stk.isEmpty()) return false;
-                if(stk.peek()=='b') stk.pop();
-                else return false;                
-            }                        
-        }
-        return stk.isEmpty();             
+    public boolean isValid(String S) {
+        String abc = "abc";
+    	
+    	while(S.contains(abc)) {
+    		S = S.replace(abc, "");
+    	}
+    	
+        return S.isEmpty();
+            
     }
 }
