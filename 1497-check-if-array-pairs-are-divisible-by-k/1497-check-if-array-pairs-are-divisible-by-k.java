071c4127cc2daeb6c1b949f179840832b42e2c4d
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-16 23:01:54 +0530

    Time: 53 ms (45.69%), Space: 89.5 MB (40.95%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -8,27 +8,16 @@ class Solution {
             freq.put(rem , freq.getOrDefault(rem,0)+1);                        
         }
         
-                
+        if(freq.getOrDefault(0,0)%2!=0) return false;
         int ans =  0;
-        for(int i=0;i<=k/2;i++){
-            int rm =  i;
-            if(rm==0 || (k%2==0 && rm==k/2 )){
-                int f =  freq.getOrDefault(rm,0);                
-                if(f%2!=0) return false;
-                ans += (f)/2;      
-                freq.remove(rm);
-            }else{
-                int t1 =   freq.getOrDefault(rm,0);
-                int t2 =   freq.getOrDefault(k-rm,0);
-                if(t1!=t2) return false;
-                ans =  ans+(t1); 
-                freq.remove(rm);
-                freq.remove(k-rm);
-                
+        for(int i=1;i<=k/2;i++){            
+                int t1 =   freq.getOrDefault(i,0);
+                int t2 =   freq.getOrDefault(k-i,0);
+                if(t1!=t2) return false;                
             }      
            
-        }
         
-        return ans==n/2;                
+        
+        return true;
     }
 }
