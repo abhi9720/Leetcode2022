d7f87457b5a10477b7cb1b80f938230bb4049fc6
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-16 11:20:13 +0530

    Time: 5 ms (43.81%), Space: 45.5 MB (26.89%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -3,33 +3,15 @@ class Solution {
         int pu =  0, po = 0;
         int n =  pushed.length;
         Stack<Integer> stk =  new Stack<>();
-        Set<Integer> used = new HashSet<>();
+        
         while( pu<n && po<n){
-            if( stk.size()>0 && stk.peek()== popped[po] ){
-                // case 1 
+            stk.push(pushed[pu]);            
+            while(stk.size()>0&& po<n && stk.peek()== popped[po]  ){
                 stk.pop();
                 po++;
-            }else{
-                if(used.contains(popped[po]) ){
-                    return false;
-                }                     
-                while(pu<n ){
-                    stk.push( pushed[pu] );
-                    used.add(pushed[pu] );                    
-                    if(pushed[pu]==popped[po] ){
-                        pu++;
-                        break;
-                    }                           
-                    pu++;
-                }                                         
-            }                                                
-        }
-        
-        while(po<n && stk.peek()== popped[po]  ){
-            stk.pop();
-            po++;
-        }        
-                
+            }    
+            pu++;
+        }                                
         return po==n;
     }
 }
