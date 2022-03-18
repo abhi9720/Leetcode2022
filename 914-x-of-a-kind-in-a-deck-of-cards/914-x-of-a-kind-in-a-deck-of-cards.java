eaf41275962654e2ce0f1586e0cf5be1a8bbb81b
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-18 23:11:25 +0530

    Time: 10 ms (56.20%), Space: 51.9 MB (37.50%) - LeetHub

---
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer,Integer> map =  new HashMap<>();
+        for(int ele : deck){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+       
+       
+        // now find gdc  -  gdc what maximum value can divide all element 
+        // number of set  = count/ gcd and size  of set =  gcd
+        int gcd =  -1;
+        for(int k  : map.keySet()){
+            gcd  = _gcd( gcd, map.get(k) ) ;
+            
+        }
+        
+        return gcd>=2;
+    }
+    
+    private static int _gcd(int n1 ,int n2){
+        if(n1==-1) return n2;
+        while(n1%n2!=0){
+            int rem  =  n1%n2;
+            n1 =  n2;
+            n2 =  rem;
+        }
+        return n2;
+    }
+}
