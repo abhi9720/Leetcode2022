b722caebb637fd2e7bb1a0a4dbbe3a617b3c4d0d
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-19 12:22:03 +0530

    Time: 5 ms (38.97%), Space: 42.9 MB (38.05%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.java
+++ 781-rabbits-in-forest/781-rabbits-in-forest.java
@@ -1,15 +1,22 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int map[] =  new int[1000];
         for(int ele : answers){
-            map.put(ele , map.getOrDefault(ele,0)+1 );
+            map[ele]+=1;
         }
         int rabbit =  0;
-        for(int key : map.keySet()){
-            int grp =  key+1;
-            int c  = map.get(key);
+        // for(int key : map.keySet()){
+        //     int grp =  key+1;
+        //     int c  = map.get(key);
+            // int count = (int) Math.ceil( (c*1.0)/grp ) * grp;
+            // rabbit+= count;
+        // }
+        
+        for(int i=0;i<1000;i++){
+            int grp  = i+1;
+            int c =  map[i];
             int count = (int) Math.ceil( (c*1.0)/grp ) * grp;
-            rabbit+= count;
+            rabbit+= count;            
         }
         return rabbit;
     }
