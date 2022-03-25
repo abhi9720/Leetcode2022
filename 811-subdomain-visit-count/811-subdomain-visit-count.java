e3ad2f300cbc5d87d3c7ed70835887dc410176c1
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-25 17:04:58 +0530

    Time: 19 ms (81.19%), Space: 50.7 MB (63.00%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for(String web : cpdomains ){
+            String website[] =  web.split(" ");
+            Integer val =  Integer.valueOf(website[0]);
+            String domain  =  website[1];
+            // start traversing form back strng if dot found , then split string 
+            int j =  domain.length();
+            int i =  j-1;
+            while(i>=-1){
+                if( i==-1 ){
+                    String subdomain  =  domain.substring(i+1,j);
+                    map.put(subdomain , map.getOrDefault(subdomain,0) +val  );                    
+                    break;
+                }
+                if(domain.charAt(i)=='.'){
+                    String subdomain  =  domain.substring(i+1,j);
+                    map.put(subdomain , map.getOrDefault(subdomain,0) +val  );                    
+                }
+                i--;
+            }            
+        }
+        ArrayList<String> ans =  new ArrayList<>();
+        for(String dom : map.keySet() ){
+            String count_paired =   map.get(dom)+" "+dom;
+            ans.add(count_paired);            
+        }
+        
+        return ans;
+    }
+}
