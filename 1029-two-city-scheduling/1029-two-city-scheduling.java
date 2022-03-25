49ffb7763bf9b655a683ffb1fcfc41c01885c5f8
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-25 21:05:10 +0530

    Time: 3 ms (45.38%), Space: 42 MB (65.41%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,5 +1,9 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
+        // pick first n/2 for which cost of traversal in city A is minimum 
+        // take diff city a cost - city b cost 
+        // and pick first n/2 people 
+        
           Arrays.sort(costs, (a, b) -> {
             return (a[0] - a[1]) - (b[0] - b[1]);
         });
@@ -9,7 +13,7 @@ class Solution {
             
             if(i<n/2){
                 cost+=c[0];
-                // System.out.println("cost : "+cost+" "+c[0])
+                
             }else{
              cost+=c[1];
             }
