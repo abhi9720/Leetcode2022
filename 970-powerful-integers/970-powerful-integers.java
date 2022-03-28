83eaf1ac94605660a07f15da90174d996b34ebfc
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-03-28 18:01:20 +0530

    Time: 2 ms (77.69%), Space: 41.8 MB (65.00%) - LeetHub

--- 970-powerful-integers/970-powerful-integers.java
+++ 970-powerful-integers/970-powerful-integers.java
@@ -1,17 +1,13 @@
 class Solution {
- public List powerfulIntegers(int x, int y, int bound) {
-List res = new ArrayList<>();
-
-    HashSet<Integer> set = new HashSet<>();
-    
-    for(int i = 1; i < bound ; i = i * x){
-        for(int j = 1; i + j <= bound ; j = j * y){
-            set.add(i + j);
-            if(y == 1){break;}
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        HashSet<Integer> ans =  new HashSet<>();
+        for(int i=1 ;i<bound ;i*=x){
+            for(int j=1;i+j<=bound;j*=y){
+                ans.add(i+j);
+                if(y==1) break;
+            }
+            if(x==1) break;
         }
-      if(x == 1){break;}
+        return new ArrayList<>(ans);
     }
-    
-    return new ArrayList<>(set);
-}
 }
