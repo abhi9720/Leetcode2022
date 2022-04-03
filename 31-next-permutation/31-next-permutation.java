74e536072e4784f37ba03cfcaa12ca618ff97b2b
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-03 18:15:59 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (90.10%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n  =  nums.length;
+        int i =  n-1;
+        int p1 =  n;
+        while(i>0){
+            if( nums[i-1] < nums[i] ){
+              p1 =  i-1;
+              break;
+            }
+            i--;
+        }
+        if(i==0){
+            reverse(nums,0,n-1);
+            return ;
+        }
+         
+        // now find point which is greater
+        int j =  n-1;
+        while( j>p1 ){
+            if( nums[j] > nums[p1] ){
+                break;
+            }
+            j-=1;
+        }
+        // swap
+        int t =  nums[p1];
+        nums[p1] =  nums[j];
+        nums[j] =  t;
+        // reverse from p+1
+        reverse(nums,p1+1,n-1);
+
+    }
+    void reverse(int nums[],int i , int j){
+        while(i<j){
+            int t =  nums[i];
+            nums[i] = nums[j];
+            nums[j] =  t;
+            i++;j--;
+        }
+    }
+}
