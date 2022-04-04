3cab4044ae65c9f264067ba988e4217cbfd080cf
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-04 14:48:19 +0530

    Time: 82 ms (11.13%), Space: 87 MB (5.28%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -1,33 +1,41 @@
 class FreqStack {
-    Map<Integer, Integer> freq;
-    Map<Integer, Stack<Integer>> group;
-    int maxfreq;
 
+    // map fre to stack
+    // map to store freq of element     
+    Map<Integer, Stack<Integer> > map1;
+    Map<Integer, Integer > map2;
+    int maxFreq = 0;
     public FreqStack() {
-        freq  = new HashMap();
-        group = new HashMap();
-        maxfreq = 0;
+        map1 = new HashMap<>();
+        map2 =  new HashMap<>();        
     }
-
-    public void push(int x) {
-        int f = freq.getOrDefault(x, 0) + 1;
-        freq.put(x, f);
-        if (f > maxfreq)
-            maxfreq = f;
-        
-        group.computeIfAbsent(f, z-> new Stack()).push(x);
+    
+    public void push(int val) {
+        map2.put( val, map2.getOrDefault( val , 0 )+1  );
+        int freq =  map2.get(val);
+        map1.putIfAbsent(freq , new Stack<Integer>() );
+        map1.get(freq).add( val );
+        maxFreq =  Math.max(maxFreq,freq);                
     }
-
+    
     public int pop() {
-        int x = group.get(maxfreq).pop();
-        freq.put(x, freq.get(x) - 1);
-        if (group.get(maxfreq).size() == 0)
-            maxfreq--;
-        return x;
+        
+        int popped = map1.get(maxFreq).pop();
+        map2.put( popped, map2.get( popped )-1  );
+        if(map2.get(popped)==0 ){
+            map2.remove(popped);
+        }
+        if(map1.get(maxFreq).size()==0){
+            map1.remove(maxFreq);
+            maxFreq-=1;
+        }
+        return popped;        
     }
 }
 
-/*
-diagram :  type this complete url 
-![image](https://assets.leetcode.com/users/images/f68273c5-4a3e-46ec-b6b3-9bca172a7422_1647686912.7624485.jpeg)
-*/
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
