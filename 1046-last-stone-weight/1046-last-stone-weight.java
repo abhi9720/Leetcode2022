b236f2be86cee730814bf2dd016575c169c3a1e2
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-08 00:24:07 +0530

    Time: 4 ms (12.15%), Space: 41.4 MB (67.48%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -7,13 +7,12 @@ class Solution {
         while(q.size()>1 ){
             int h1 =  q.poll();
             int h2 =  q.poll();
-            if(h1==h2) {                
-                continue;}
-            int newStone  =  Math.abs(h1-h2);
+            
+            int newStone  = h1-h2;
             
             q.offer(newStone);
          
         }
-        return q.size()==0?0: q.peek();
+        return   q.peek();
     }
 }
