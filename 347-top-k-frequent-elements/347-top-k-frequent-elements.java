24654732c6d0092f00b56ce3e37b237c9146becf
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-09 19:39:28 +0530

    Time: 24 ms (22.16%), Space: 50.9 MB (29.21%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,39 @@
+class Solution {
+    class pair{
+        int ele ;
+        int freq;
+        public pair(int ele , int freq){
+            this.ele  =  ele;
+            this.freq =  freq;            
+        }
+        
+    }
+    class pairComparator implements Comparator<pair>{
+         public int compare(pair p1, pair p2) {
+             return p1.freq - p2.freq;
+         }
+        
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        
+        HashMap<Integer,Integer> map =  new HashMap<>();
+        for(int ele : nums){
+            map.put( ele , map.getOrDefault(ele,0)+1 );
+        }
+        
+        PriorityQueue<pair> q =  new PriorityQueue<>(new pairComparator());
+        for(int key : map.keySet() ){
+            q.add( new pair(key, map.get(key) )  ) ;
+            if(q.size() > k ){
+                q.poll();
+            }
+        }
+        int ans[] =  new int[k];
+        int idx = 0;
+        while(q.size()>0 ){
+            ans[idx++] =  q.poll().ele;
+        }
+        return ans;
+    }
+}
