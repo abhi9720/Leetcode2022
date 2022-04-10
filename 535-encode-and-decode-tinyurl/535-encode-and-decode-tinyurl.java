7c262501130c8e79727dad1c40b119dc1d6ce9f4
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-10 18:23:01 +0530

    Time: 5 ms (60.55%), Space: 43.7 MB (48.33%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,33 +1,29 @@
 public class Codec {
 
+  HashMap<String, String> map;
+    public Codec(){
+        map = new HashMap<>();
+    }
     
-    static String charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    HashMap<String, String> urlToCode =  new HashMap<>();
-    HashMap<String,String> codeToUrl =  new HashMap<>();
+    // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        if(urlToCode.containsKey(longUrl) ){
-            return urlToCode.get(longUrl);
+        StringBuilder sb = new StringBuilder();
+        Random r = new Random();
+        char ch = (char)r.nextInt(100);
+        sb.append(ch);
+        
+        while(map.containsKey(sb.toString()) == true){
+            ch = (char)r.nextInt(100);
+            sb.append(ch);
         }
         
-        StringBuilder sb =  new StringBuilder( );
-        String key =  "";
-        do{
-            for(int i=0;i<6;i++){
-                int idx =  (int)Math.random()*charSet.length();
-                sb.append( charSet.charAt(idx) );
-            }
-            
-            key  =  sb.toString();
-        }
-        while( codeToUrl.containsKey(key) );
-        urlToCode.put(longUrl,key);
-        codeToUrl.put(key,longUrl);
-        return key;
+        map.put(sb.toString(), longUrl);
+        return sb.toString();
     }
 
-    // Decodes a shortened URL to its original URL .
+    // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return codeToUrl.get(shortUrl);
+        return map.get(shortUrl);
     }
 }
 
