b99d62cf5408633d6bd842cae7e59284e4349eb7
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-10 22:34:55 +0530

    Time: 5 ms (66.03%), Space: 50 MB (7.86%) - LeetHub

---
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        
+        PriorityQueue<Integer> pq =   new PriorityQueue<>(Collections.reverseOrder());
+        
+        int petrol =  startFuel;;
+        int count  = 0,i=0;
+        while(petrol < target){
+            while(i<stations.length){
+                if(stations[i][0] <= petrol){
+                    pq.add(stations[i][1]);
+                }else{
+                    break;
+                }
+                i++;
+            }
+            if(pq.size()==0 ){
+                return -1;
+            }
+            petrol+=pq.poll();
+            count++;
+        }
+        
+      
+        return count;
+        
+        
+        
+        
+        
+        
+    }
+}
