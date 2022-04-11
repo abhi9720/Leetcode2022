376afcd00a4eba4b2dfc7d8b904e32847749bdd3
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-11 12:33:51 +0530

    Time: 1 ms (95.07%), Space: 41.8 MB (78.58%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -1,42 +1,42 @@
 class Solution {
-    class pair implements Comparable<pair>{
-        char ele;
-        int freq ;
-        public pair(char ele , int freq){
-            this.ele =  ele;
-            this.freq =  freq;
-        }        
-        public int compareTo(pair p){
-            return p.freq - this.freq;
-        }
-    }
     public String reorganizeString(String s) {
-        PriorityQueue<pair> pq =  new PriorityQueue<>();
         int freq[] =  new int[26];
         for(char ch : s.toCharArray() ){
             freq[ch-'a']++;
         }
-        // now store in pq 
+        
+        int max = 0, maxLetter = 0;
         for(int i=0;i<26;i++){
-            if(freq[i]!=0 ){
-                char ch  =  (char)('a'+i);
-                pq.add(new pair(ch , freq[i]) );                
+            if(freq[i] >max ){
+                maxLetter =  i;
+                max = freq[i];
             }
         }
-        // now start making string 
-        StringBuilder sb = new StringBuilder();
-        pair prev =  null;
-        while(!pq.isEmpty() ){
-            pair p =  pq.remove();
-            sb.append( p.ele );
-            p.freq -=1;
+        
+        if(max > (s.length()+1 )/2 ){
+            return "";
+        }
+        
+        
+        int idx = 0;
+        char sb[] =  new char[s.length() ];
+        while(freq[maxLetter] >0 ){
+            sb[idx] = (char)('a' + maxLetter);
+            idx+=2;
+            freq[maxLetter]-=1;
+        }
+        
+        for(int i=0;i<26;i++){
+            while(freq[i] >0 ){
+                if(idx >= sb.length ){
+                    idx = 1;
+                }
+                sb[idx] =  (char)('a'+i) ;
+                idx+=2;
+                freq[i]-=1;                
+            }
+        }                        
+        return String.valueOf(sb);
             
-            if(prev!=null && prev.freq!=0){
-                pq.offer(prev);
-            }            
-            prev = p;
-        }        
-        if(prev!=null && prev.freq!=0 ) return "";
-        return sb.toString();                    
     }
 }
