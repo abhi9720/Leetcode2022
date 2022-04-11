e5b0e716858907bd6f0777be6cc5c980c577fc26
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-11 11:53:42 +0530

    Time: 4 ms (66.17%), Space: 42.6 MB (32.48%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    class pair{
+        char ele;
+        int freq ;
+        public pair(char ele , int freq){
+            this.ele =  ele;
+            this.freq =  freq;
+        }        
+    }
+    public String reorganizeString(String s) {
+        PriorityQueue<pair> pq =  new PriorityQueue<>( (a,b)->{
+            return b.freq -  a.freq;
+        } );
+        int freq[] =  new int[26];
+        for(char ch : s.toCharArray() ){
+            freq[ch-'a']++;
+        }
+        // now store in pq 
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0 ){
+                char ch  =  (char)('a'+i);
+                pq.add(new pair(ch , freq[i]) );                
+            }
+        }
+        // now start making string 
+        StringBuilder sb = new StringBuilder();
+        pair prev =  null;
+        while(!pq.isEmpty() ){
+            pair p =  pq.remove();
+            sb.append( p.ele );
+            p.freq -=1;
+            
+            if(prev!=null && prev.freq!=0){
+                pq.offer(prev);
+            }            
+            prev = p;
+        }        
+        if(!pq.isEmpty() || prev!=null && prev.freq!=0 ) return "";
+        return sb.toString();                    
+    }
+}
