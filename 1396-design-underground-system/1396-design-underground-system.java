9f39de28cc86ef9e69083dfe1a2276cd1108f043
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-24 14:09:44 +0530

    Time: 158 ms (53.65%), Space: 102.7 MB (37.68%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,24 +1,78 @@
-class UndergroundSystem {
-    HashMap<String, Pair<Integer, Integer>> checkoutMap = new HashMap<>(); // Route - {TotalTime, Count}
-    HashMap<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();  // Uid - {StationName, Time}
-    
-	public UndergroundSystem() {}
-    
-	public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new Pair<>(stationName, t));
+class customerData{
+    int t;
+    String st;
+    public customerData(int t , String st){
+        this.t =  t;
+        this.st =  st;
     }
-    
-	public void checkOut(int id, String stationName, int t) {
-        Pair<String, Integer> checkIn = checkInMap.get(id);
-        String route = checkIn.getKey() + "_" + stationName;
-        int totalTime = t - checkIn.getValue();
-        Pair<Integer, Integer> checkout = checkoutMap.getOrDefault(route, new Pair<>(0, 0));
-        checkoutMap.put(route, new Pair<>(checkout.getKey() + totalTime, checkout.getValue() + 1));
-    }
-    
-	public double getAverageTime(String startStation, String endStation) {
-        String route = startStation + "_" + endStation;
-        Pair<Integer, Integer> checkout = checkoutMap.get(route);
-        return (double) checkout.getKey() / checkout.getValue();
+    public String toString(){
+        return "["+t+"@"+st+"], ";
     }
 }
+class avgObject{
+    double avg;
+    int c;
+    
+    public String toString(){
+        return "["+avg+"@"+c+"], ";
+    }
+    public avgObject(int c, double time){
+        this.c =  c;
+        this.avg  =  time;
+    }
+    void updateAvg(double newavg){        
+        this.avg =  newavg;
+        this.c+=1;        
+    }    
+}
+class UndergroundSystem {
+
+    
+    final HashMap<Integer,customerData> train ;
+    final Map<String,HashMap<String,avgObject >> record ;
+    
+    public UndergroundSystem() {
+        train =  new HashMap<>();
+        record =  new HashMap<>();
+    }
+    
+    public void checkIn(int id, String stationName, int t) {
+      
+        customerData newcust =  new customerData(t,stationName);
+        train.put(id,newcust);
+        
+
+        
+    }
+    
+    public void checkOut(int id, String stationName, int t) {
+        customerData cust =  train.get(id);
+        String startStation =  cust.st;
+        int duration =  t - cust.t;
+        record.putIfAbsent(startStation,new HashMap<>());
+        HashMap<String,avgObject > endstation = record.get(startStation);
+        avgObject e = endstation.get(stationName);
+        if(e!=null){            
+            double newavg =  ((e.avg*e.c ) + duration)/(e.c+1);
+            e.updateAvg(newavg);
+        }
+        else{
+            avgObject entry =  new avgObject(1,duration);
+            endstation.put(stationName,entry);
+            
+        }
+        train.remove(id);             
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        HashMap<String,avgObject > station =  record.get(startStation);
+        avgObject entry =  station.get(endStation);
+        return entry.avg;
+       
+    }
+    
+    
+    
+    
+}
+
