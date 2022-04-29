47469f6c18306ad22b36357d6e25029a129b5555
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-04-29 22:06:33 +0530

    Time: 9 ms (10.85%), Space: 42.4 MB (99.48%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -0,0 +1,56 @@
+class pair{
+    int v,l;
+    public pair(int v,int l){
+        this.v =  v;
+        this.l  = l;
+    }
+    @Override
+    public String toString(){
+        return v+"@"+l+", ";
+    }
+}
+ 
+class Solution {
+    
+    public boolean isBipartite(int[][] graph) {
+        int n =  graph.length;
+        Integer visited[]= new Integer[n];
+        
+        
+        
+        for(int i=0;i<n;i++){
+            if(visited[i]==null && !isBipartite(graph,visited,i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    boolean isBipartite(int [][]graph, Integer visited[], int src ){
+        ArrayDeque<pair> q =  new ArrayDeque<>();
+        q.add(new pair(src,1) );
+        while(q.size() >0 ){
+            pair rm = q.remove();
+            if(visited[rm.v]!=null ){
+                
+                // means we are visiting same vertex again
+                // check its level same or not
+                int orginalLevel =  visited[rm.v];
+                int newLevel = rm.l;
+                
+                if(orginalLevel!=newLevel) return false;
+            }
+            
+            visited[rm.v] = rm.l;
+            for(int nbr:graph[rm.v]){
+                if(visited[nbr]==null)
+                q.add(new pair(nbr,rm.l+1) );
+            }   
+            
+        }
+        return true;        
+    }
+    
+    
+    
+}
