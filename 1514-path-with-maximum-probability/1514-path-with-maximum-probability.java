f80808b433536abe9773d2b4456078aec623f966
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-02 17:15:34 +0530

    Time: 59 ms (66.23%), Space: 74.9 MB (62.57%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -0,0 +1,67 @@
+class Solution {
+    
+     class Edge{
+        int v,nbr;
+        double p;
+       public Edge(int v, int nbr, double p) {
+            this.v =  v;
+            this.nbr =  nbr;
+            this.p = p;
+        }        
+    }
+    
+     class mover implements Comparable<mover>{
+        int v;
+        double psf;
+        mover(int v,double psf){
+            this.v =  v;
+            this.psf =  psf;
+        }
+        public int compareTo(mover o){
+            return Double.compare( o.psf,this.psf);
+        }
+    }
+    
+    public  double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        
+        ArrayList<Edge>[] graph = new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+        
+        for (int i = 0; i < edges.length; i++) {
+            int v1 =  edges[i][0], v2 =  edges[i][1];
+            double p =  succProb[i];
+            graph[v1].add(new Edge(v1, v2, p));
+            graph[v2].add(new Edge(v2, v1, p));            
+        }
+        
+        return dijkstra(graph,n,start,end);
+
+        
+    }
+    
+    private  double dijkstra(ArrayList<Edge>[] graph,int vtces,int start,int end){
+        
+        PriorityQueue<mover> q =  new PriorityQueue<>();
+        boolean visited[] =  new boolean[vtces];
+        q.offer(new mover(start,1));
+        while(q.size()>0 ) {
+        	mover peek =  q.remove();
+        	if(peek.v==end) return peek.psf;
+        	if(visited[peek.v]) continue;
+        	visited[peek.v] = true;
+        	// now explore its neighbour
+        	
+        	for(Edge ed : graph[peek.v]) {
+        		if(!visited[ed.nbr] ) {
+        			q.offer(new mover(ed.nbr,peek.psf*ed.p) );
+        		}
+        	}        	        	
+        }
+        
+        
+        return 0.0;
+    }
+    
+}
