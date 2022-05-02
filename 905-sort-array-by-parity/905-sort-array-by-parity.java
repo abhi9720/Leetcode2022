2e84bd993a7d1eff96e1a86a6a6d44bcb76538a7
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-02 08:59:18 +0530

    Time: 2 ms (38.15%), Space: 46.6 MB (72.88%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,15 +1,31 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int arr[] =  new int[nums.length];
-        int idx1 = 0, idx2 = nums.length-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0 ){
-                arr[idx1++] =  nums[i];
+        // int arr[] =  new int[nums.length];
+        // int idx1 = 0, idx2 = nums.length-1;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]%2==0 ){
+        //         arr[idx1++] =  nums[i];
+        //     }
+        //     else{
+        //         arr[idx2--] =  nums[i];
+        //     }
+        // }
+        // return arr;
+        // with out extra space 
+        int i =  0, e = 0;
+        while(i<nums.length){
+            if(nums[i]%2==0){
+                swap(nums,i,e);                
+                e++;
             }
-            else{
-                arr[idx2--] =  nums[i];
-            }
+            i++;
+                
         }
-        return arr;
+        return nums;
     }
+    private void swap(int arr[], int i, int j){
+            int t =  arr[i];
+            arr[i]  =  arr[j];
+            arr[j] =  t;
+        }
 }
