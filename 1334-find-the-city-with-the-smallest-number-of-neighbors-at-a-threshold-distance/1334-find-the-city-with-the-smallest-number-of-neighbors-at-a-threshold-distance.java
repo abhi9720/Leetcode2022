7acb9348a6a4013081952e02dcdd4e5b4872b887
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-03 13:27:04 +0530

    Time: 155 ms (15.72%), Space: 54.7 MB (8.17%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,79 +1,40 @@
-// The Reason of DFS Not Working (Explain Graph and Example)
-// https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/discuss/490555/The-Reason-of-DFS-Not-Working-(Explain-Graph-and-Example)
-
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        
-        ArrayList<int []>[]graph =  new ArrayList[n];
-        for(int i=0;i<n;i++){
-            graph[i] =  new ArrayList<>();
+        Map<Integer, Map<Integer, Integer>> g = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            g.put(i, new HashMap<>());
         }
-        for(int i=0;i<edges.length;i++){            
-            int v1=  edges[i][0];
-            int v2 =  edges[i][1];
-            int wt =  edges[i][2];
-            graph[v1 ].add(new int[]{v1,v2,wt});            
-            graph[v2 ].add(new int[]{v2,v1,wt});            
-            
+        for (int[] e : edges) {
+            g.get(e[0]).put(e[1], e[2]);
+            g.get(e[1]).put(e[0], e[2]);
         }
-        int node = -1;
-        int ans =  Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            int count = bfs(n,graph,i,distanceThreshold);            
-            if(ans >=count){
-                node =  i;
-                ans =  count;                    
+        int min = n + 1;
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            Queue<int[]> q = new PriorityQueue<>((a, b)->(b[1] - a[1]));
+            q.add(new int[]{i, distanceThreshold});
+            Set<Integer> visited = new HashSet<>();
+            int count = 0;
+            while (!q.isEmpty()) {
+                int[] city = q.poll();
+                if (!visited.contains(city[0])) {
+                    visited.add(city[0]);
+                    count++;
+                } else {
+                    continue;
+                }
+                Map<Integer, Integer> m = g.get(city[0]);
+                for (int neighbor : m.keySet()) {
+                    if (!visited.contains(neighbor) && city[1] >= m.get(neighbor)) {
+                        q.add(new int[]{neighbor, city[1] - m.get(neighbor)});
+                    }
+                }
+            }
+            if (count - 1 <= min) {
+                min = count - 1;
+                res = i;
             }
         }
-       
-        
-        return node;
-        
-        
-        
-    }
-    
-    
-    private int bfs(int n , ArrayList<int []>[]graph, int src,int vo  ){
-        Queue<int[]> q =  new ArrayDeque<>();
-        int distTillNow[]= new int[n];        
-        Arrays.fill(distTillNow,-1);
-        
-        q.add(new int[]{src,0});
-        int count = 0;
-        while(q.size() >0 ){
-            int peek[] =  q.remove(); 
-            int node =  peek[0] , dist = peek[1];   
-            
-            // if threshold crossed then continue , no reason to explore in this path
-            if(dist > vo ) continue;
-            
-            // if this node previously visited, then only visit it again if we 
-           //  visiting it in smaller distance
-            if(distTillNow[node] != -1 ){
-                if( dist > distTillNow[node]    ) continue;
-            }
-            
-            // increase count only if first time visiting
-            if(distTillNow[node] == -1){
-                count++;
-            }
-            
-            distTillNow[node] = dist;                                        
-                                    
-            for(int ed[]:graph[peek[0]]){
-                if(dist+ed[2] <= vo) q.add(new int[]{ed[1],dist+ed[2]});                
-            }                     
-        }
-        
-        return count-1;
-        
+        return res;
     }
 }
-
-
-
-
-
-
-
