34455be2d4541c773edc8b3341151be9887cc0b8
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-05 16:52:02 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean visited[] = new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(cycle(adj,visited,i,-1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean cycle(ArrayList<ArrayList<Integer>> graph,boolean visited[] ,int src, int parent){
+        visited[src] =  true;
+        
+        for(Integer nbr : graph.get(src) ){
+            if(!visited[nbr]  ){
+                if( cycle(graph,visited,nbr,src)) return true;
+            }
+            else if(parent!=nbr){
+                // System.out.println(parent+"  "+nbr);
+                return true;
+            }
+        }
+        return false;
+    }
+}
