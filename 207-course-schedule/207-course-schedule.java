9123d79096ced6964e0430652fbed64febb264ed
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-06 13:50:39 +0530

    Time: 3 ms (96.22%), Space: 47.3 MB (68.27%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        // if there is cycle in graph then no answer possible 
+        ArrayList<Integer>[] graph =  new ArrayList[n];
+        for(int i=0;i<graph.length;i++){
+            graph[i] =  new ArrayList<>();
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            int v1 =  prerequisites[i][0], v2 =  prerequisites[i][1];
+            graph[v1].add( v2 );
+        }        
+        boolean visited[] =  new boolean[n];
+        boolean dfsVisit[]= new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(isCycle(graph,visited,dfsVisit,i,-1)){
+                    return false;
+                }
+            }
+        }
+        return true;        
+    }
+    
+    // detect cycle in direct graph
+    private boolean isCycle(ArrayList<Integer>[] graph, boolean visited[] , boolean dfsVisit[],int src, int parent){
+        visited[src]  =  true;
+        dfsVisit[src] =  true;
+        
+        // explore nbr
+        for(int nbr : graph[src]){
+            if(!visited[nbr] ){
+                if(isCycle(graph,visited,dfsVisit,nbr,src)) return true;
+            }
+            else if(dfsVisit[nbr]){
+                return true;
+            }
+        }
+        
+        dfsVisit[src] =  false;
+        return false;
+        
+    }
+    
+}
