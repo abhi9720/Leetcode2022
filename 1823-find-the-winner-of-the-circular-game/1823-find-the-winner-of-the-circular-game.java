58f1ea48aaaddaec46e0e10403db90e7437586ae
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-11 22:56:00 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (74.99%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        return solution(n,k)+1;
+    }
+    
+    public static int solution(int n, int k){
+    if(n==0) return n;
+    int toadd =  k%n;
+    int left  =  solution(n-1,k);
+    return (left+toadd)%n;
+    
+  }
+}
