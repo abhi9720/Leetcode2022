b2faef0400e89cbbda33d020804af77975ee4c88
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-11 12:30:05 +0530

    Time: 0 ms (100.00%), Space: 41 MB (35.01%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -16,9 +16,7 @@ class Solution {
             prevCol =  newCol;
             previousSum =  sum;
         }
-        
-        
-        
+                        
         return previousSum;
         
         
