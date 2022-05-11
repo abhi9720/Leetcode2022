8e4322522328c3874f1770880e93c0f692fdab17
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-11 12:25:20 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (56.06%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -2,24 +2,22 @@ class Solution {
     public int countVowelStrings(int n) {
         //represent a,e,i,o,u - > 0,1,2,3,4
         // [5][-]  - total sum
-        int arr[][] =  new int[6][n+1];
-        for(int i=0;i<=4;i++){
-            arr[i][1] =  1;
-        }
-        arr[5][1] =  5;
-        for(int j=2;j<=n;j++){
-            arr[0][j] =  arr[5][j-1];
-            int c = 0,sum=arr[0][j] ;
+        int arr[][] =  new int[5][n+1];                
+        int previousSum = 1;
+        for(int j=1;j<=n;j++){
+            arr[0][j] =  previousSum;
+            int c = 0,sum= previousSum ;
             for(int i=1;i<=4;i++){
                 c+=arr[i-1][j-1];
                 arr[i][j] =  arr[0][j] - c;
                 sum+=arr[i][j];
             }            
-            arr[5][j] =  sum;
+            previousSum =  sum;
         }
         
         
-        return arr[5][n];
+        
+        return previousSum;
         
         
         
