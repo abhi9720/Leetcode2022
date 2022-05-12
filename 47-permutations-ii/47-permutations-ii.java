48211685e0245d8646805ab7f4a5af9fa1273f79
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-12 23:09:08 +0530

    Time: 6 ms (45.88%), Space: 48 MB (50.76%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<Integer> permu  =  new ArrayList<>();
+        List<List<Integer> > ans =  new ArrayList<>();
+        HashMap<Integer,Integer> map =  new HashMap<>();
+        Set<Integer> unique =  new HashSet<>();
+        for(int ele : nums){            
+            unique.add(ele);
+            map.putIfAbsent(ele,0);
+            map.put(ele,map.get(ele)+1);
+        }
+        
+        helper(map,permu,ans,nums.length,unique);
+        return ans;
+        
+    }
+    private void helper(HashMap<Integer,Integer> map ,List<Integer> permu ,List<List<Integer> > ans ,int n,Set<Integer> unique){
+        if(permu.size() == n ){
+            ans.add(new ArrayList(permu) );
+            return;
+        }
+        for(int key : unique){
+            int count =  map.get(key);
+            if(count>0 ){
+                map.put(key,count-1 );
+                permu.add(key);
+                helper(map,permu,ans,n,unique);
+                permu.remove(permu.size()-1 );
+                map.put(key,count );                    
+            }            
+        }
+        
+    }
+}
