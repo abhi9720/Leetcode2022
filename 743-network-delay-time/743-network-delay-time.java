3baae952b1b7a118892a7e4666d45b466bc12c05
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-14 09:43:19 +0530

    Time: 14 ms (86.03%), Space: 46.7 MB (79.86%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -1,6 +1,7 @@
 class Solution {
     
     public int networkDelayTime(int[][] times, int n, int k) {
+        // src, nbr, time
         ArrayList<int []>[] graph = new ArrayList[n+1];
         
         for (int i = 0; i <= n; i++) {
@@ -12,17 +13,23 @@ class Solution {
             int src =  times[i][0];
             graph[src].add( times[i] );
         }
+        
         int time = 0;
         PriorityQueue<int []> q =new PriorityQueue<>( (o1,o2)-> o1[1] - o2[1] );
+        //node, time 
+        
         boolean visited[] =  new boolean[n+1];
-        q.add( new int[]{k,0});
+        q.add( new int[]{k,0});// node,time to reach this node
         while(q.size()>0 ){
             int peek[] =  q.remove();
             if(visited[peek[0]] ) continue;
+            
+            // time delayed is maximum time taken to reach node
             time  =  Math.max(peek[1], time );
+            
             visited[peek[0]] =  true;
             for(int []ed : graph[peek[0]]){
-                if(!visited[ed[1]] ){ // src, nbr,time
+                if(!visited[ed[1]] ){ 
                     q.offer(new int[]{ed[1],peek[1]+ed[2]});
                 }
             }            
