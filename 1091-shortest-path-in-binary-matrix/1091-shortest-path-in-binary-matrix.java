4887fa7f60c48c85f7eb875be3366c44a0953554
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-16 08:50:33 +0530

    Time: 39 ms (26.82%), Space: 68.2 MB (23.23%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -11,12 +11,13 @@ class Solution {
             int x =  temp[0],  y =  temp[1] , cost = temp[2];
             if(visited[x][y] ) continue;
             visited[x][y] =  true;
-            
             if(x==n-1 && y==m-1) return cost;
+            
             
             for(int d[] : dir){
                 int nx =  d[0]+x , ny =  d[1]+y;
-                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0 ){
+                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0 && !visited[nx][ny]  ){
+                    if(nx==n-1 && ny==m-1) return cost+1;
                     q.add(new int[]{nx,ny,cost+1});
                 }
             }
