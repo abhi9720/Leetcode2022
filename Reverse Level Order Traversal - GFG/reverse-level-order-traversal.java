7c1ccf2033c657db7ad09ad9d7c5ce944136488a
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-16 22:06:51 +0530

    Added solution - LeetHub

---
+++ Reverse Level Order Traversal - GFG/reverse-level-order-traversal.java
@@ -0,0 +1,165 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            ArrayList<Integer> ans = g.reverseLevelOrder(root);
+
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+ 
+// } Driver Code Ends
+
+
+
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        Queue<Node> q =  new LinkedList<>();
+        q.add(node);;
+        q.add(null);
+        ArrayList<Integer> res  = new ArrayList<Integer>();
+        ArrayList<Integer> level  = new ArrayList<Integer>();
+        
+        Stack<ArrayList<Integer>> stk  =  new Stack<>();
+        
+        while(q.size()>1 ){
+            Node peek  =  q.remove();
+            if(peek==null){
+                res.addAll(0,new ArrayList<>(level) );
+                level =   new ArrayList<Integer>();
+                q.add(null);
+                continue;
+            }
+            
+            level.add(peek.data);
+            if(peek.left!=null){
+                
+                q.add(peek.left);
+            }
+            if(peek.right!=null){
+                
+                q.add(peek.right);
+            }
+        }
+        res.addAll(0,new ArrayList<>(level) );
+        
+        
+        
+        
+        return res;
+        
+    }
+}      
