672e39bd1db2a1e9ea2461634a8f44734991b919
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-17 16:06:44 +0530

    Time: 10 ms (43.34%), Space: 44.9 MB (63.68%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -0,0 +1,53 @@
+
+class pair implements Comparable<pair> {
+    String s;
+    int count;    
+    public pair(String s,int count) {
+        this.s =  s;
+        this.count =  count;
+    }
+    public int compareTo( pair o ){
+        if(o.count==this.count){
+            return this.s.compareTo(o.s);
+        }else{
+            return o.count  -  this.count;
+        }
+    }
+}
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String str : words ){
+            map.putIfAbsent(str,0);
+            map.put(str,map.get(str)+1 );
+        }
+        PriorityQueue<pair> pq =  new PriorityQueue<>( 
+            (o1,o2)-> o2.count -  o1.count
+        );
+        for(String key : map.keySet() ){
+            pq.add(new pair( key , map.get(key) ) );            
+        }
+        map.clear();
+        List<pair> ans =  new ArrayList<>();
+        int loop = k;
+        pair peek = null ;
+        while(loop-->0 || ( pq.size()>0 && (peek.count == pq.peek().count) ) ){
+            ans.add( pq.peek() );
+            peek =  pq.remove();
+        }
+        pq.clear();
+        Collections.sort(ans);
+        
+        List<String> res =  new ArrayList<>();
+        loop = k;
+        for(pair p : ans){
+            res.add(p.s);
+            k--;
+            if(k==0) break;
+        }
+        
+        
+        return res;
+        
+    }
+}
