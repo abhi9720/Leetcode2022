7292b003b0df220a3bac01e6bb58409ee1089bfa
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-20 15:21:40 +0530

    Time: 1 ms (43.34%), Space: 42.5 MB (21.92%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -1,49 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stk =  new Stack<>();
         List<Integer> list =  new ArrayList<>();
-        TreeNode curr =  root;
-		while(curr!=null) {
-			TreeNode left =  curr.left;
-			if(left==null) {
-				list.add(curr.val);
-				curr =  curr.right;
-			}
-			else {
-				while(left.right!=null && left.right!=curr) {
-					left =  left.right;
-				}
-				
-				if(left.right==curr) {		
-					//break thread 
-					
-					left.right =  null;
-					curr =  curr.right;
-				
-				}
-				else {
-					// create thread 
-                    list.add(curr.val);
-					left.right =  curr;
-					curr =  curr.left;
-				}
-			}
-			
-		}
+        while(stk.size() >0  || root!=null){
+            while(root!=null){
+                stk.add(root);
+                list.add(root.val);
+                root = root.left;
+            }
+            TreeNode peekNode =  stk.pop();
+            root =  peekNode.right;
+            
+        }
         return list;
     }
 }
