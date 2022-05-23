d76ad077d12cec048142e5d61b292a5cf5fa7a76
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-23 17:00:33 +0530

    Time: 45 ms (64.14%), Space: 43.3 MB (58.42%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,32 +1,53 @@
 /*
-
 For dp[i][j][k], we can get it by fetching the current string i or discarding the current string, which would result in dp[i][j][k] = dp[i-1][j-numOfZero(strs[i])][i-numOfOnes(strs[i])] and dp[i][j][k] = dp[i-1][j][k]; We only need to treat the larger one in it as the largest number for dp[i][j][k].
-
 */
 class Solution {
       public int findMaxForm(String[] strs, int m, int n) {
-          int l = strs.length;
-        int[][] dp = new int[m+1][n+1];
+//         int l = strs.length;
+//         int[][][] dp = new int[l+1][m+1][n+1];
 
-        for (int i = 0; i < m+1; i++) {
-            Arrays.fill(dp[i], 0);
-        }
+//         for (int i = 0; i < l+1; i++) {
+//             int[] nums = new int[]{0,0};
+//             if (i > 0) {
+//                 nums = calculate(strs[i-1]);
+//             }
+//             for (int j = 0; j < m+1; j++) {
+//                 for (int k = 0; k < n+1; k++) {
+//                     if (i == 0) {
+//                         dp[i][j][k] = 0;
+//                     } else if (j>=nums[0] && k>=nums[1]) {
+//                         dp[i][j][k] = Math.max(dp[i-1][j][k], dp[i-1][j-nums[0]][k-nums[1]]+1);
+//                     } else {
+//                         dp[i][j][k] = dp[i-1][j][k];
+//                     }
+//                 }
+//             }
+//         }    
 
-        int[] nums = new int[]{0,0};
-        for (String str : strs) {
-            nums = calculate(str);
-            for (int j = m; j >= nums[0]; j--) {
-                for (int k = n; k >= nums[1]; k--) {
-                    if (j>=nums[0] && k>=nums[1]) {
-                        dp[j][k] = Math.max(dp[j][k], dp[j-nums[0]][k-nums[1]]+1);
-                    } else {
-                        dp[j][k] = dp[j][k];
-                    }
+//         return dp[l][m][n];
+          
+              int l = strs.length;
+    int[][] dp = new int[m+1][n+1];
+    
+    for (int i = 0; i < m+1; i++) {
+        Arrays.fill(dp[i], 0);
+    }
+    
+    int[] nums = new int[]{0,0};
+    for (String str : strs) {
+        nums = calculate(str);
+        for (int j = m; j >= nums[0]; j--) {
+            for (int k = n; k >= nums[1]; k--) {
+                if (j>=nums[0] && k>=nums[1]) {
+                    dp[j][k] = Math.max(dp[j][k], dp[j-nums[0]][k-nums[1]]+1);
+                } else {
+                    dp[j][k] = dp[j][k];
                 }
             }
         }
-
-        return dp[m][n];
+    }
+    
+    return dp[m][n];
     }
 
     private int[] calculate(String str) {
