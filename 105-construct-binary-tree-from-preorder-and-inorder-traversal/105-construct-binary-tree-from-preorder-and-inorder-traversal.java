d7b26b210a4b848d0553851c6dc7ff1215a8ea08
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-23 23:03:21 +0530

    Time: 2 ms (94.67%), Space: 42 MB (85.51%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,29 +13,35 @@
  *     }
  * }
  */
-class Solution{
-     public static TreeNode buildTree(int[] pre, int[] in) {
-        
-        return helper(pre,in,0,pre.length-1,0,in.length-1);
-     }
-    
-    public static TreeNode helper(int[] pre, int[] in, int sp,int ep,int si,int ei) {
-        
-        
-        if(sp > ep || si >ei){
+class Solution {
+
+    public static TreeNode buildTree(int[] pre, int[] in) {
+        // hashmap only work for unique entry of binary tree
+        // i.e., no two nodes of same data
+        HashMap<Integer,Integer> inordermap =  new HashMap<>();
+        for(int i=0;i<in.length;i++){
+            inordermap.put(in[i], i );
+        }
+        return helper(pre, in, 0, pre.length - 1, 0, in.length - 1,inordermap);
+    }
+
+    public static TreeNode helper(int[] pre, int[] in, int sp, int ep, int si, int ei ,
+                                 HashMap<Integer,Integer> inordermap
+                                 ) {
+        if (sp > ep || si > ei) {
             return null;
         }
-        
-        // make root 
-        TreeNode root = new TreeNode(pre[sp] );
-        int count = 0;
-        for(int i =  si;i<=ei;i++){
-            if(in[i] == pre[sp] ) break;
-            count++;
-        }
-        
-        root.left  =  helper(pre,in,sp+1,sp+count,si,si+count-1);
-        root.right = helper(pre,in,sp+count+1,ep,si+count+1,ei);
+
+        // make root
+        TreeNode root = new TreeNode(pre[sp]);
+        int count = inordermap.get(pre[sp]) - si ;
+        // for (int i = si; i <= ei; i++) {
+        //     if (in[i] == pre[sp]) break;
+        //     count++;
+        // }
+
+        root.left = helper(pre, in, sp + 1, sp + count, si, si + count - 1,inordermap);
+        root.right = helper(pre, in, sp + count + 1, ep, si + count + 1, ei,inordermap);
         return root;
     }
 }
