39efb9d6f9659d2ecd92914090b9841f3105e1f9
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-23 23:24:05 +0530

    Time: 3 ms (77.69%), Space: 44.6 MB (33.26%) - LeetHub

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+      public static TreeNode buildTree(int[] inorder, int[] postorder) {
+    HashMap<Integer,Integer> map  =  new HashMap<>();
+    for(int i=0;i<inorder.length;i++){
+        map.put(inorder[i], i );
+    }
+    
+    return helper(inorder,postorder,0,inorder.length-1,0,postorder.length-1,map);
+  }
+  
+  private static TreeNode helper(int in[],int post[],int si,int ei,int sp,int ep,HashMap<Integer,Integer>map){
+      if(sp>ep || si > ei){
+          return null;
+      }
+      TreeNode root =  new TreeNode(post[ep]);
+      int count = ei -  map.get( post[ep] );
+      
+      root.left = helper(in,post,si,map.get( post[ep] )-1, sp , ep-count-1 , map);
+      root.right = helper(in,post,map.get( post[ep] )+1,ei,ep-count , ep-1 , map);
+      return root;
+  }
+}
