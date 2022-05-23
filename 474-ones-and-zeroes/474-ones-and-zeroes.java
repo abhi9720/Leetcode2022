343f154f9c3047d4c9103bc1469cfb2b43a8c063
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-23 17:01:33 +0530

    Time: 57 ms (56.55%), Space: 41.5 MB (95.96%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -26,28 +26,27 @@ class Solution {
 
 //         return dp[l][m][n];
           
-              int l = strs.length;
-    int[][] dp = new int[m+1][n+1];
-    
-    for (int i = 0; i < m+1; i++) {
-        Arrays.fill(dp[i], 0);
-    }
-    
-    int[] nums = new int[]{0,0};
-    for (String str : strs) {
-        nums = calculate(str);
-        for (int j = m; j >= nums[0]; j--) {
-            for (int k = n; k >= nums[1]; k--) {
-                if (j>=nums[0] && k>=nums[1]) {
-                    dp[j][k] = Math.max(dp[j][k], dp[j-nums[0]][k-nums[1]]+1);
-                } else {
-                    dp[j][k] = dp[j][k];
+        int l = strs.length;
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 0; i < m+1; i++) {
+            Arrays.fill(dp[i], 0);
+        }
+
+        int[] nums = new int[]{0,0};
+        for (String str : strs) {
+            nums = calculate(str);
+            for (int j = m; j >= nums[0]; j--) {
+                for (int k = n; k >= nums[1]; k--) {
+                    if (j>=nums[0] && k>=nums[1]) {
+                        dp[j][k] = Math.max(dp[j][k], dp[j-nums[0]][k-nums[1]]+1);
+                    } else {
+                        // dp[j][k] = dp[j][k];
+                    }
                 }
             }
         }
-    }
-    
-    return dp[m][n];
+        return dp[m][n];
     }
 
     private int[] calculate(String str) {
