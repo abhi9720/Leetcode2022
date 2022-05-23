4867c5fd2ab91a793fa0d416e4157f46341734b6
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-23 22:55:14 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (70.21%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -1,8 +1,15 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
         
-        if(columnNumber<=26){            
-            return ""+(char)('A'+columnNumber-1);
+       return helper(columnNumber).toString();
+    }
+    private StringBuilder helper(int columnNumber){
+        
+        if(columnNumber<=26){        
+            StringBuilder sb =  new StringBuilder();
+            char ch = (char)('A'+columnNumber-1);
+            sb.append(ch);
+            return sb;
         }
         int mod =  columnNumber%26;
         int nextnum =  columnNumber/26;
@@ -10,7 +17,11 @@ class Solution {
             mod =  26;
             nextnum-=1;
         }
-        return   convertToTitle(nextnum)+(char)('A'+mod-1);
+        StringBuilder sb =  new StringBuilder();
+        sb.append(helper(nextnum));
+        sb.append( (char)('A'+mod-1) );
+        
+        return sb;
         
     }
 }
