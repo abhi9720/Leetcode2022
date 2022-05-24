788503d65e491e5387c5edda82c0a7c5211ec1ea
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-24 10:42:47 +0530

    Time: 9 ms (25.84%), Space: 44.3 MB (8.72%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -8,11 +8,18 @@ class Solution {
                 stk.push(i);
             }
             else{
-                stk.pop();
+                stk.pop(); // here we removed open bracket
+                // if there is no opening bracket in stack
+                // then stack will be empty and push current ith index 
                 if(stk.empty() ){
                     stk.push(i);
                 }
                 else{
+                    // as we popped open bracket
+                    // we are willing to find , max len ending at
+                    // index i
+                    // here ith index is closing bracket
+                    // that means , max len at ith closing bracket
                     maxLen =  Math.max(maxLen, i -  stk.peek() );
                 }
             }
