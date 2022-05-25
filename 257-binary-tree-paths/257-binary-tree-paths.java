f7d787bac9561017032be7221e2d109ee8565e6c
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-25 12:24:46 +0530

    Time: 9 ms (56.82%), Space: 42.2 MB (96.53%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -9,11 +9,11 @@ class Solution {
     private void dfs(TreeNode root, List<String> ans, String path) {
         if (root == null) return;
         if (root.left == null && root.right == null) {
-            path  +=  (path.length() == 0 ? "" : "->") + root.val;
+            path  +=   root.val;
             ans.add(path);
             return;
         }
-        path  +=  (path.length() == 0 ? "" : "->") + root.val;
+        path  +=   root.val+"->";
         
         dfs(root.left, ans, path);
         dfs(root.right, ans, path);
