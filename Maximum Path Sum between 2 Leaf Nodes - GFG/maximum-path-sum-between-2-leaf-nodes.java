def4e6a838f7d8a07e0f9f05e6be7f99a0134a13
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-29 18:55:21 +0530

    Added solution - LeetHub

--- Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.java
+++ Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.java
@@ -117,39 +117,56 @@ class GFG {
 } */
 class Solution
 {
-    int max; //  maximum l 2 l
-    int maxPathSum(Node root)
-    { 
-        max = Integer.MIN_VALUE;
+    static int maxLeafPath =  Integer.MIN_VALUE;
+    int maxPathSum(Node root){ 
+        maxLeafPath = Integer.MIN_VALUE;
+        
+        int ans = dfs(root);
 
-
-        if(root.left != null && root.right != null){
-            helper(root);
-            return max;
-        } else {
-            // because this question on GFG considers root with one child as  a leaf
-            int val = helper(root);
-            return Math.max(val, max);
-        }
+        
+        
+		if(root.left==null || root.right==null){
+		    return Math.max(maxLeafPath,ans);
+		};
+		
+        
+		return maxLeafPath;
     } 
-    
-    // it returns maximum root to leaf
-    int helper(Node node){
-      if(node.left != null && node.right != null){
-          int left = helper(node.left);
-          int right = helper(node.right);
-
-          max = Math.max(max, left + node.data + right);
-
-          return Math.max(left, right) + node.data;
-      } else if(node.left != null){
-          int left = helper(node.left);
-          return node.data + left;
-      } else if(node.right != null){
-          int right = helper(node.right);
-          return node.data + right;
-      } else {
-          return node.data;
+	private int dfs(Node root){
+		
+		if(root.left==null && root.right==null){
+		    return root.data;
+		}
+		else if(root.left==null){
+		    int r =  dfs(root.right);
+		    return r+root.data;
+		}
+		else if(root.right==null){
+		    int l =  dfs(root.left);
+		    return l+root.data;
+		}
+		else {
+		    int l =  dfs(root.left), r =  dfs(root.right);
+		    int leafPath =  l+r+root.data;
+		  //  System.out.println("At "+root.data+" -> "+leafPath+" => "+maxLeafPath);
+		    if(leafPath> maxLeafPath) maxLeafPath =  leafPath;
+		    return Math.max(l,r)+root.data;
+		}
+		
+		
+	}
+	
+	 Node setTree(Node root){
+       
+      Node temp = new Node(0);
+      //if tree is left most
+      if(root.right==null){
+          root.right=temp;
       }
+      else{    //if tree is right most
+          root.left=temp;
+      }
+       
+      return root;
     }
 }
