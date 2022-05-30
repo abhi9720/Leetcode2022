188030a17ee90bacac30eb1c92ea7b2733b18b95
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-30 16:29:42 +0530

    Time: 3 ms (62.04%), Space: 44.8 MB (71.83%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.java
@@ -1,49 +1,106 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-	int counter =1;
-	int maxCouter  = 0;
-	int prev = -(int)1e4;
+
     public int[] findMode(TreeNode root) {
-        counter =0;
-	    maxCouter  = 0;
-	    prev = -(int)1e4;
-        TreeNode temp =  root;
-        while(temp!=null && temp.left!=null){
-            temp = temp.left;
+        // morris traversal
+        int prev = -1;
+        int counter = 0;
+        int maxCouter = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while (curr != null) {
+            TreeNode left = curr.left;
+            if (curr.left == null) {
+                                   
+                    // inorder visit
+                    
+//    ------- Checking previous code and update list START                
+                    if (prev == -1 || prev == curr.val) counter++; else counter = 1;
+                    // System.out.println(prev+" => "+curr.val+" , "+counter+" , "+maxCouter);
+                    
+                    if (counter > maxCouter) {
+                        maxCouter = counter;
+                        ans.clear();
+                        ans.add(curr.val);
+                    } else if (counter == maxCouter) {
+                        if (ans.size() > 0) {
+                            if (ans.get(ans.size() - 1) != curr.val) {
+                                ans.add(curr.val);
+                            }
+                        }
+                    }
+//    ------- Checking previous code and update list END
+                prev = curr.val;
+                
+                
+                curr = curr.right;
+            } else {
+                while (left.right != null && left.right != curr) {
+                    left = left.right;
+                }
+                if (left.right == curr) { // helps in deciding now we have to move right
+                    
+                    // inorder visit
+                    
+//    ------- Checking previous code and update list START      
+                    
+                    if (prev == -1 || prev == curr.val) counter++; else counter = 1;
+                    // System.out.println(prev+" => "+curr.val+" , "+counter+" , "+maxCouter);
+                    
+                    if (counter > maxCouter) {
+                        maxCouter = counter;
+                        ans.clear();
+                        ans.add(curr.val);
+                    } else if (counter == maxCouter) {
+                        if (ans.size() > 0) {
+                            if (ans.get(ans.size() - 1) != curr.val) {
+                                ans.add(curr.val);
+                            }
+                        }
+                    }
+//    ------- Checking previous code and update list END
+                    prev = curr.val;
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    left.right = null;
+                    curr = curr.right;
+                } else {
+                    left.right = curr;
+                    curr = curr.left;
+                }
+            }
         }
-        prev =  temp.val;
-        ArrayList<Integer> ans =  new ArrayList<>();
-        inorder(root,ans);
+        
+        
         int res[] =  new int[ans.size() ];
         for(int i=0;i<ans.size();i++ ){
             res[i] = ans.get(i);
         }
         return res;
+        
+        
     }
-    private void inorder(TreeNode root, ArrayList<Integer> ans){
-        
-		if(root==null) return ;
-        
-		inorder(root.left,ans);
-        
-        // System.out.println(prev+"  "+root.val);
-		if(prev==root.val) counter++;
-        else counter = 1;
-		if(counter>maxCouter){
-			maxCouter  = counter;
-			ans.clear();
-			ans.add(root.val);
-		}
-		else if(counter==maxCouter){
-			if(ans.size() >0 ){
-				if(ans.get(ans.size()-1 ) != root.val ){
-					ans.add(root.val);
-				}
-			}
-		}
-		prev =  root.val;
-		inorder(root.right,ans);
-		
-
-
-	}
 }
