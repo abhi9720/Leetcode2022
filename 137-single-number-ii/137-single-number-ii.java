0bd3fd6ffe6003284432d0d0f0c274d44adf591a
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-05-31 14:37:02 +0530

    Time: 1 ms (91.86%), Space: 43.7 MB (66.32%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -1,20 +1,30 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// https://leetcode.com/problems/maximum-number-of-consecutive-values-you-can-make/
+
 class Solution {
     public int singleNumber(int[] nums) {
-        
-        // count bits 
-        // 0,1,2,3,4,5,6,7, ............. 31
-        int num  =0;
-        for(int i=0;i<32;i++){            
-            int mask = (1<<i);
-            int bitCount = 0;
-            for(int ele :nums){
-                bitCount+= ( (ele & mask) != 0 ?1:0  );
-            }
-            
-            if(bitCount%3!=0){
-                num = num | (1<<i);
-            }
+        int _3n=-1, _3np1=0,_3np2=0;
+        for(int ele : nums){
+            int commonWith3n = ele & _3n;
+			int commonWith3np1 =  ele & _3np1;
+			int commonWith3np2 =  ele & _3np2;
+
+			// so the bits common with 3n will be off, 
+			// because after adding ele to themm 
+			// their count goes to _3np1 type
+			_3n = _3n^commonWith3n;// off in 3n
+			_3np1 = _3np1 | commonWith3n;// on in 3n+1;
+
+			_3np1 = _3np1 ^ commonWith3np1; // off in 3np1
+			_3np2 = _3np2 | commonWith3np1; // on in 3np2
+
+			_3np2 = _3np2 ^ commonWith3np2; // off in 3np2
+			_3n = _3n | commonWith3np2; // on in 3n
         }
-        return num;
+
+		return _3np1;
+        
+        
+ 
     }
 }
