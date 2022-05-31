ef468d969a73aa24d4bcfc0c07db3a60894c4e75
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-01 00:10:29 +0530

    Time: 0 ms (100.00%), Space: 49.1 MB (67.95%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -1,29 +1,32 @@
 
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null) return root;
+		if(root==null) return null;
         if(root.val==key){
-         if(root.left==null) return root.right;
-         else if(root.right==null) return root.left;
-         else{
-          TreeNode del =  root.right;
-          while(del.left!=null){
-              del =  del.left;
-          }
-          root.val  =  del.val;
-          root.right = deleteNode(root.right,del.val);
-          
-         }
+            if(root.left==null) return root.right;
+			else if(root.right==null) return root.left;
+			else{
+				// both left and right not null
+				TreeNode successor = root.right;
+				while(successor.left!=null){
+					successor =  successor.left;
+				}
+				root.val  =  successor.val;
+				root.right =  deleteNode(root.right,successor.val);
+				
+
+			}
         }
-        else if(root.val > key){// move left
-            root.left= deleteNode(root.left,key);
-        }
-        else{
-            root.right=  deleteNode(root.right,key);
-        }
+        else if(root.val < key){
+			// move right
+			root.right =  deleteNode(root.right,key);
+
+		}
+		else{
+			// move left
+			root.left =  deleteNode(root.left,key);
+		}
+    
         return root;
     }
-    
-    // to delete a node fo tree replace that node with the
-    // right tree leftMost Node 
 }
