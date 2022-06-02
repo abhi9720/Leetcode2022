f3fc6551ac7c889eb4de2bf58afa2cfb04a71c33
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-02 08:59:17 +0530

    Time: 55 ms (9.94%), Space: 68.8 MB (54.97%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -1,47 +1,31 @@
-// aproach 1 
-// get left tree inorder
-// get right tree inorder
-// then Use algo merge two sorted array
-        
-// Approach 2 by maintaining two stacks
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
 		List<Integer> ans =  new ArrayList<>();
         Stack<TreeNode> stk1 =  new Stack<>(),
-						stk2 =  new Stack<>();
-		
-		TreeNode p1  =  root1 , p2 =  root2;
-		while(p1!=null){
-			stk1.push(p1);
-			p1 =  p1.left;
-		}
-		while(p2!=null){
-			stk2.push(p2);
-			p2 =  p2.left;
-		}
-		while(stk1.size() > 0 || stk2.size() >0 ){
-			
+						stk2 =  new Stack<>();						
+		pushLeftMost(root1,stk1);
+        pushLeftMost(root2,stk2);
+        
+		while(stk1.size() > 0 || stk2.size() >0 ){			
             if( stk2.size() ==0 ||  (stk1.size()!=0 && ( stk1.peek().val < stk2.peek().val)) ){
 				TreeNode peekNode =  stk1.pop();
-				ans.add(peekNode.val);
-				peekNode =  peekNode.right;
-				while(peekNode!=null){
-					stk1.push(peekNode);
-					peekNode =  peekNode.left;
-				}
+				ans.add(peekNode.val);				
+				pushLeftMost(peekNode.right,stk1);
 			}
 			else {
 				TreeNode peekNode =  stk2.pop();
-				ans.add(peekNode.val);
-				peekNode =  peekNode.right;
-				while(peekNode!=null){
-					stk2.push(peekNode);
-					peekNode =  peekNode.left;
-				}
+				ans.add(peekNode.val);				
+				pushLeftMost(peekNode.right,stk2);
 			}
 
 		}	
 		return ans;        
     }
-    
+            
+    private void pushLeftMost(TreeNode p , Stack<TreeNode> stk){
+        while(p!=null){
+			stk.push(p);
+			p =  p.left;
+		}
+    }
 }
