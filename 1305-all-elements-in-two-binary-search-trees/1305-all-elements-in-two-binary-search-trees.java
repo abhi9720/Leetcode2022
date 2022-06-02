7b8654fcc91d97d163822a6cfdf8b4fe71fadb52
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-02 08:54:06 +0530

    Time: 44 ms (19.70%), Space: 73.6 MB (9.75%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,53 @@
+// aproach 1 
+// get left tree inorder
+// get right tree inorder
+// then Use algo merge two sorted array
+        
+// Approach 2 by maintaining two stacks
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+		List<Integer> ans =  new ArrayList<>();
+        Stack<TreeNode> stk1 =  new Stack<>(),
+						stk2 =  new Stack<>();
+		
+		TreeNode p1  =  root1 , p2 =  root2;
+		while(p1!=null){
+			stk1.push(p1);
+			p1 =  p1.left;
+		}
+		while(p2!=null){
+			stk2.push(p2);
+			p2 =  p2.left;
+		}
+		while(stk1.size() > 0 || stk2.size() >0 ){
+			
+            if( stk2.size() ==0 ||  (stk1.size()!=0 && ( stk1.peek().val < stk2.peek().val)) ){
+				TreeNode peekNode =  stk1.pop();
+				ans.add(peekNode.val);
+				peekNode =  peekNode.right;
+				while(peekNode!=null){
+					stk1.push(peekNode);
+					peekNode =  peekNode.left;
+				}
+			}
+			else {
+				TreeNode peekNode =  stk2.pop();
+				ans.add(peekNode.val);
+				peekNode =  peekNode.right;
+				while(peekNode!=null){
+					stk2.push(peekNode);
+					peekNode =  peekNode.left;
+				}
+			}
+
+		}
+		while(!stk1.isEmpty() ){
+			ans.add(stk1.pop().val);
+		}
+		while(!stk2.isEmpty() ){
+			ans.add(stk2.pop().val);
+		}
+		return ans;        
+    }
+    
+}
