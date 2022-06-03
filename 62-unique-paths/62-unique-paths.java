01feab862cb0ea450a15a029375c70d7f1f5166f
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-03 22:52:03 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (33.09%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,16 +1,21 @@
-class Solution {
+public class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][]  =  new int[m][n];
-        Arrays.fill(dp[m-1],1);
-        for(int i=0;i<m;i++){
-            dp[i][n-1]  = 1;
+        if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
         }
-        
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                dp[i][j] =  dp[i][j+1] + dp[i+1][j];
-            }
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
         }
-        return dp[0][0];
+            
+        return (int)res;
     }
 }
