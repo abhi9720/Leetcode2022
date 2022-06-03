09e90e08c0e43670b95d65887e96428ae43e1a58
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-03 20:17:17 +0530

    Time: 1 ms (77.09%), Space: 42 MB (66.45%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -5,27 +5,17 @@ class Solution {
         
     }
     private double helper(double x, int n){
-        if(n<0){
-            if(n==-1){
-                return 1/x;
-            }
-            else{
-                double ans =  helper(x,n/2);
-                if(n%2==0) return ans*ans;
-                else{
-                    return ans*ans/x;
-                }
-            }
+        if(n==-1) return 1/x;            
+        if(n==0) return 1;
+        
+        double ans =  helper(x,n/2);
+        if(n%2==0) return ans*ans;
+        else if(n<0){
+            return ans*ans/x;            
         }
         else{
-            if(n==0) return 1;
-            double ans =  helper(x,n/2);
-            if(n%2==0){
-                return ans*ans;
-            }
-            else{
-                return ans*ans*x;
-            }
+            return ans*ans*x;
         }
+        
     }
 }
