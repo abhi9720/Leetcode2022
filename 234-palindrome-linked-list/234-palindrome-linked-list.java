638aa14f3f4f2018c888f78061287677179fec5e
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-06 13:30:36 +0530

    Time: 5 ms (85.78%), Space: 97.2 MB (56.14%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -1,49 +1,76 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 class Solution {
-    ListNode front=null;    
+    ListNode front = null;
+
     public boolean isPalindrome(ListNode head) {
-        // recursive 
+        // recursive
         // this.front =  head;
         // return helper(head);
-        return isPalin(head);
+        // return isPalin(head);
+        return isPalindrome_withoutStack(head);
     }
+
     // @ Recursive
-    private boolean helper(ListNode head){
-        if(head==null) return true;
-        if(!helper(head.next) ) return false;
-        if(head.val!=front.val) return false;
-        front =  front.next;        
+    private boolean helper(ListNode head) {
+        if (head == null) return true;
+        if (!helper(head.next)) return false;
+        if (head.val != front.val) return false;
+        front = front.next;
         return true;
     }
-    // @ Itertative 
-    private boolean isPalin(ListNode head){
-        Stack<ListNode> stk =  new Stack<>();
-        ListNode f =  head, s =  head;
-        while(f!=null && f.next!=null){
+
+    // @ Itertative
+    private boolean isPalin_stack(ListNode head) {
+        Stack<ListNode> stk = new Stack<>();
+        ListNode f = head, s = head;
+        while (f != null && f.next != null) {
             stk.push(s);
-            f =  f.next.next;            
-            s =  s.next;            
+            f = f.next.next;
+            s = s.next;
         }
-        
-        ListNode itr =  s;
-        if(f!=null)itr =  itr.next;
-        
-        while(stk.size() >0 ){
-            if(stk.pop().val !=itr.val ){
+
+        ListNode itr = s;
+        if (f != null) itr = itr.next;
+
+        while (stk.size() > 0) {
+            if (stk.pop().val != itr.val) {
                 return false;
             }
-            itr =  itr.next;
+            itr = itr.next;
         }
         return true;
+    }
+
+    // @ Itertative
+    public boolean isPalindrome_withoutStack(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
