587e7966219b4e0e6e6e8ba3f9bb54e55a245caa
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-06 13:28:53 +0530

    Time: 28 ms (16.89%), Space: 107.6 MB (17.14%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -12,15 +12,38 @@
 class Solution {
     ListNode front=null;    
     public boolean isPalindrome(ListNode head) {
-        this.front =  head;
-        return helper(head);
+        // recursive 
+        // this.front =  head;
+        // return helper(head);
+        return isPalin(head);
     }
-    
+    // @ Recursive
     private boolean helper(ListNode head){
         if(head==null) return true;
         if(!helper(head.next) ) return false;
         if(head.val!=front.val) return false;
         front =  front.next;        
+        return true;
+    }
+    // @ Itertative 
+    private boolean isPalin(ListNode head){
+        Stack<ListNode> stk =  new Stack<>();
+        ListNode f =  head, s =  head;
+        while(f!=null && f.next!=null){
+            stk.push(s);
+            f =  f.next.next;            
+            s =  s.next;            
+        }
+        
+        ListNode itr =  s;
+        if(f!=null)itr =  itr.next;
+        
+        while(stk.size() >0 ){
+            if(stk.pop().val !=itr.val ){
+                return false;
+            }
+            itr =  itr.next;
+        }
         return true;
     }
 }
