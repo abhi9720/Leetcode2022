fc3477d77e6fb19fc0e33c26653ea11a8c13c3c1
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-06 21:28:22 +0530

    Time: 26 ms (42.00%), Space: 51.8 MB (56.06%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -8,51 +8,45 @@
  * }
  */
 public class Codec {
-
-    // Encodes a tree to a single string.
+    
     public String serialize(TreeNode root) {
-        StringBuilder sb =  new StringBuilder();
+        StringBuilder sb  =  new StringBuilder();        
         serializeHelper(root,sb);
-        int size =  sb.length();
-        
-        // sb.deleteCharAt(size-1);
-        
+        // delete last char
+        sb.setLength( Math.max(0,sb.length()-1 ) );
         return sb.toString();
-        
     }
     private void serializeHelper(TreeNode root,StringBuilder sb){
-        if(root==null) return ;
-        
+        if(root==null){ 
+            sb.append("#,") ;
+            return ;
+        }
         sb.append(root.val+",");
-        
-        if(root.left==null) sb.append("#,");
-        else serializeHelper(root.left,sb);                
-        if(root.right==null) sb.append("#,");
-        else serializeHelper(root.right,sb);        
+        serializeHelper(root.left,sb);
+        serializeHelper(root.right,sb);                
     }
-    int idx = 0;
-    TreeNode deserializeHelper(String nums[]){
-        if(idx>=nums.length) return null;
-        if(nums[idx].trim().equals("#") ){
-            
-            idx+=1;
+    int idx =  0;
+    public TreeNode deserialize(String data) {
+        if(data==null || data.length()==0 ) return null;
+        this.idx = 0;
+        String nodes[] =  data.split(",");
+        return deserializeHelper(nodes);
+    }
+    private TreeNode deserializeHelper(String data[]){
+        if(idx==data.length) return null;
+        if(data[idx].equals("#") ) {
+            idx++;
             return null;
         }
-        // System.out.println("nums[idx] : "+nums[idx]);
-        TreeNode root =  new TreeNode(Integer.parseInt(nums[idx] ) );
+        TreeNode root =  new TreeNode(Integer.parseInt(data[idx]) );
         idx+=1;
-        // System.out.println("root.val : "+root.val);
-        root.left = deserializeHelper(nums);
-        root.right = deserializeHelper(nums);
-        return root;
+        root.left = deserializeHelper(data);
+        root.right = deserializeHelper(data);
+        return root;            
     }
-    
-    public TreeNode deserialize(String data) {
-        String nums[] =  data.split(",");
-        if(nums.length==1 ) return null;
-        
-        return  deserializeHelper(nums);
-        
-    }
-    
 }
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
