9e4e5adb3b582680ce305e0301a4e5d9d2baa982
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-07 13:01:57 +0530

    Time: 1 ms (54.54%), Space: 42 MB (70.55%) - LeetHub

--- 86-partition-list/86-partition-list.java
+++ 86-partition-list/86-partition-list.java
@@ -8,26 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
+
     public ListNode partition(ListNode head, int x) {
-        ListNode l1 =  new ListNode(-1) , t1=  l1;
-        ListNode l2 =  new ListNode(-1), t2 =  l2;
-        
-        for(ListNode ptr =  head ; ptr!=null;ptr =  ptr.next){            
-            if(ptr.val < x){               
-                t1.next =  ptr;
-                t1 = ptr;                                    
+        ListNode h1 = null, t1 = null;
+        ListNode h2 = null, t2 = null;
+        ListNode temp = head;
+        while (temp != null) {
+            if (temp.val < x) {
+                if (h1 == null) {
+                    h1 = temp;
+                    t1 = temp;
+                } else {
+                    t1.next = temp;
+                    t1 = temp;
+                }
+                temp = temp.next;
+                t1.next = null;
+            } else {
+                if (h2 == null) {
+                    h2 = temp;
+                    t2 = temp;
+                } else {
+                    t2.next = temp;
+                    t2 = temp;
+                }
+                temp = temp.next;
+                t2.next = null;
             }
-            else{              
-                t2.next =  ptr;
-                t2 = ptr;                    
-                
-            }            
         }
-        t1.next =  l2.next;
-        t2.next = null;
-        return l1.next;
-        
+        if (h1 == null) {
+            return h2;
+        } else {
+            t1.next = h2;
+            return h1;
+        }
     }
 }
