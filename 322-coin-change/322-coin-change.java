77359e5467345bb4def0c878b1e18c8e4681a27a
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-09 19:31:26 +0530

    Time: 40 ms (31.83%), Space: 58.6 MB (9.93%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,22 +1,21 @@
-public class Solution {
-public int coinChange(int[] coins, int amount) {
-    if(amount<1) return 0;
-    int[] dp = new int[amount+1];
-    int sum = 0;
-    // dp[i] means minimum coins required to make amount i , 
-    // then using dp[i] , we can calculate dp[i+1] , using all coins 
-    // starting from sum =1 ; means minimum coins required to make amount 1
-	while(++sum<=amount) {
-		int min = -1;
-    	for(int coin : coins) {
-    		if(sum >= coin && dp[sum-coin]!=-1) {
-    			int temp = dp[sum-coin]+1;
-                // if min== -1 then fill temp otherwise compare , minimum(min,temp)
-    			min = min<0 ? temp : Math.min(temp,min);
-    		}
-    	}
-    	dp[sum] = min;
-	}
-	return dp[amount];
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        int prev[] = new int[amount+1];
+        Arrays.fill(prev,Integer.MAX_VALUE-10);
+        prev[0] =  0;
+        for(int i=0;i<coins.length;i++){
+            int curr[] =  new int[amount+1];
+            for(int amt=1;amt<=amount;amt++ ){
+                if(amt< coins[i] ){
+                    curr[amt] =  prev[amt];
+                }
+                else{
+                    curr[amt] =  Math.min( prev[amt], 1+curr[amt -coins[i] ] );
+                }
+            }            
+            prev =  curr;
+        }
+        return prev[amount]==2147483637?-1:prev[amount];
     }
 }
