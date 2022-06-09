38baede1c315335289c3b6016d204b31fb0ef461
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-09 19:48:18 +0530

    Time: 39 ms (32.68%), Space: 58.1 MB (12.22%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,7 +2,7 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         
         int prev[] = new int[amount+1];
-        Arrays.fill(prev,Integer.MAX_VALUE-10);
+        Arrays.fill(prev,-1);
         prev[0] =  0;
         for(int i=0;i<coins.length;i++){
             int curr[] =  new int[amount+1];
@@ -11,11 +11,27 @@ class Solution {
                     curr[amt] =  prev[amt];
                 }
                 else{
-                    curr[amt] =  Math.min( prev[amt], 1+curr[amt -coins[i] ] );
+                    // curr[amt] =  Math.min( prev[amt], 1+curr[amt -coins[i] ] );
+                    int without =  prev[amt];
+                    int with =  curr[amt -coins[i] ];
+                    // System.out.println(without+"  "+with);
+                    if(with==-1){
+                        curr[amt]=  without;
+                    }
+                    else if(without==-1){
+                        curr[amt] = 1+with;
+                    }
+                    else{
+                        curr[amt] =  Math.min(without,1+with);
+                    }
+                    // curr[amt] =  without==-1? with : Math.min(without,with);
+                    // System.out.println(coins[i]+" , "+amt+"   => "+ curr[amt] );
+                    // System.out.println("----------------------");
                 }
             }            
             prev =  curr;
         }
-        return prev[amount]==2147483637?-1:prev[amount];
+        
+        return prev[amount];
     }
 }
