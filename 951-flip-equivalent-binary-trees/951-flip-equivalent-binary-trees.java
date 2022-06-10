fadfcce5f8964dea94f66a91be86f769d50ad149
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-10 23:11:14 +0530

    Time: 0 ms (100.00%), Space: 42 MB (33.76%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.java
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.java
@@ -5,14 +5,13 @@ class Solution {
         return dfs(root1, root2);
     }
 
-    private boolean dfs(TreeNode root1, TreeNode root2) {        
-        if (root1 == null || root2 == null) return root1==root2;
-        if (root1.val != root2.val) {            
-            return false;
-        }
+    private boolean dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true; 
+        else if (root1 == null || root2 == null) return false; 
+        else if (root1.val != root2.val) return false;
 
-        return dfs(root1.left,root2.left) && dfs(root1.right,root2.right) ||
-            dfs(root1.left,root2.right) && dfs(root1.right,root2.left);
-        
+        return 
+            dfs(root1.left, root2.left) && dfs(root1.right, root2.right) 
+            || dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
     }
 }
