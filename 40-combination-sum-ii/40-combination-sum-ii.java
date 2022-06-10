5aae06e0cc65f424a2ae893f4b27eb2a2621a2eb
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-10 11:09:53 +0530

    Time: 4 ms (85.05%), Space: 44.3 MB (21.68%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -10,16 +10,17 @@ e.g 1(0th index) + 7(5th index)
     again if we going to try combination then with 1st index 1 -> then [1,7] going to be repeated 
 
 
+
 */
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans =  new ArrayList<>();
         Arrays.sort(candidates);
-        combinationSum2(candidates,target,0,ans,new ArrayList<>() );
+        combinationSum2(candidates,target,0,ans,new LinkedList<>() );
         return ans;
     }
     private void combinationSum2(int []cand, int target, int idx, List<List<Integer>> ans ,
-                                                                  ArrayList<Integer> curr  ){
+                                                                  LinkedList<Integer> curr  ){
         
         int n  =  cand.length;
         if(idx>=n || target <=0 ){
@@ -31,9 +32,9 @@ class Solution {
         for(int i=idx;i<n;i++){
             if(i>idx && cand[i] == cand[i-1] ) continue;
             if(target < cand[i]) break;
-                curr.add(cand[i] );
+                curr.addLast(cand[i] );
                 combinationSum2(cand, target-cand[i], i+1, ans, curr);
-                curr.remove(curr.size()-1 );
+                curr.removeLast( );
             
         }
         
