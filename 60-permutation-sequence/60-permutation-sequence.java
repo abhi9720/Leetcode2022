b21accf7e15da787623cc5714ac5be8787da9154
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-10 13:58:25 +0530

    Time: 2 ms (86.59%), Space: 41.6 MB (53.43%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -8,15 +8,30 @@ class Solution {
             nums.add(i);        
         }
         nums.add(n);        
+        // factorial one less than from n beacause 
+        /*
+            [1,2,3,4 ] , k =  17
+            1 +( 2,3,4 ) => taking 1 as fixed , 6 permuatation possible
+            2 +( 1,3,4 ) => taking 1 as fixed , 6 permuatation possible
+            3 +( 1,2,4 ) => taking 1 as fixed , 6 permuatation possible
+            and 17th permuation lies in this range ^  , taking 3 as fixed
+                                                   
+                                                   
+            4 +( 1,2,3 ) => taking 1 as fixed , 6 permuatation possible
+            
+            next set k =  
+        */
+
         StringBuilder sb =  new StringBuilder();
         k =  k-1;
         while(true){
-            sb.append( nums.get( k / fact ) );
-            nums.remove( nums.get( k / fact )  );
+            int idx = k/fact;
+            sb.append( nums.get(idx) );
+            nums.remove( nums.get( idx )  );
             if(nums.size()==0 ){
                 break;
             }
-            k =  k%fact;
+            k =  k%fact;            
             fact =  fact / nums.size();
         }
         
