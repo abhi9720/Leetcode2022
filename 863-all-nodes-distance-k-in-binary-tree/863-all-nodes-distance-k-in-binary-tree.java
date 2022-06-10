fcf3a2c1f781ed7108a59e864e48a46167633910
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-10 09:02:46 +0530

    Time: 10 ms (98.27%), Space: 42.2 MB (85.63%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -7,69 +7,55 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+// a node is at distance d from target node ,then all its right  clildren at distance k-d-2 will be k 
+//  distance from target node
 class Solution {
-
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        ArrayList<Integer> output = new ArrayList<>();
-        printNodesAtDistanceK(root, target, k, output);
-        return output;
+        List<Integer> ans =  new ArrayList<>();
+        printNodeDistanceK(root,target,k,ans);
+        return ans;
     }
-
-   
-    public int printNodesAtDistanceK(TreeNode root, TreeNode target, int k, ArrayList<Integer> output) {
-        if (root == null || k < 0) {
+    private int printNodeDistanceK(TreeNode root, TreeNode target, int k, List<Integer>ans){
+        
+        if(root==null || k<0){
             return -1;
         }
-
-        if (root.val == target.val) {
-            printAtDepthK(root, k, output);
+        if(root == target){
+            printNodeDepthK(root,k,ans);
             return 0;
         }
-
-        // depth left 
-        int dl = printNodesAtDistanceK(root.left, target, k, output);
-
-        if (dl != -1) {
-            if (dl + 1 == k) { // if root is also at k distance aways from target node then 
-                // also add root 
-                output.add(root.val);
-            } else {
-                // -2 because of one vertices from right and 1 vetices from left => count
-                printAtDepthK(root.right, k - dl - 2, output);
+        
+        // given me the distance on left of target Node
+        int ld =  printNodeDistanceK(root.left,target,k,ans);
+        if(ld!=-1){
+            if(ld+1==k){
+                ans.add(root.val);
             }
-            return dl + 1;
-        }
-
-        // depth right 
-        int dr = printNodesAtDistanceK(root.right, target, k, output);
-
-        if (dr != -1) {
-            if (dr + 1 == k) {// if root is also at k distance aways from target node then 
-                // also add root 
-                output.add(root.val);
-            } else {
-                // -2 because of one vertices from right and 1 vetices from left => count
-                printAtDepthK(root.left, k - dr - 2, output);
+            else{
+                printNodeDepthK(root.right,k-ld-2,ans);
             }
-            return dr + 1;
+            return ld+1;
         }
-
+        int rd =  printNodeDistanceK(root.right,target,k,ans);
+        if(rd!=-1){
+            if(rd+1==k){
+                ans.add(root.val);
+            }
+            else{
+                printNodeDepthK(root.left,k-rd-2,ans);
+            }
+            return rd+1;
+        }              
         return -1;
     }
-    
-    
-     public static void printAtDepthK(TreeNode root, int k, ArrayList<Integer> output) {
-        if (root == null || k < 0) {
-            return;
+    private void printNodeDepthK(TreeNode root, int k,List<Integer>ans){
+        if(root==null || k<0) return ;
+        if(k==0){
+            ans.add(root.val);
+            return ;
         }
-
-        if (k == 0) {
-            output.add(root.val);
-            return;
-        }
-
-        printAtDepthK(root.left, k - 1, output);
-        printAtDepthK(root.right, k - 1, output);
+        printNodeDepthK(root.left,k-1,ans);
+        printNodeDepthK(root.right,k-1,ans);
     }
-
 }
