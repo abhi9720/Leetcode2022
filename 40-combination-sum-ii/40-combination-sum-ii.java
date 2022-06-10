c66a57ef9444dc98bef862e322f26a1983f1de6a
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-10 10:29:06 +0530

    Time: 9 ms (27.62%), Space: 44.1 MB (32.08%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,42 @@
+
+/*
+[1,1,2,5,6,7,10]
+
+here for 0th index element(1), we tried all possible combinations 
+so we donot need to try again same combination with 1st index  element(1) ,
+as this will result in adding duplicay
+
+e.g 1(0th index) + 7(5th index)
+    again if we going to try combination then with 1st index 1 -> then [1,7] going to be repeated 
+
+
+*/
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans =  new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSum2(candidates,target,0,ans,new ArrayList<>() );
+        return ans;
+    }
+    private void combinationSum2(int []cand, int target, int idx, List<List<Integer>> ans ,
+                                                                  ArrayList<Integer> curr  ){
+        
+        int n  =  cand.length;
+        if(idx>=n || target <=0 ){
+            if(target==0){
+                ans.add(new ArrayList<>(curr) );
+            }
+            return ;
+        }
+        for(int i=idx;i<n;i++){
+            if(i>idx && cand[i] == cand[i-1] ) continue;
+            
+                curr.add(cand[i] );
+                combinationSum2(cand, target-cand[i], i+1, ans, curr);
+                curr.remove(curr.size()-1 );
+            
+        }
+        
+                
+    }
+}
