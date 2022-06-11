256e81b5d3e1453765848a4c1b7abf02fa922d6d
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-11 09:25:11 +0530

    Time: 9 ms (24.07%), Space: 60.1 MB (22.80%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
@@ -1,18 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+/*
+Break our problem to smaller problem
+like for n =  7
+    left root right
+    1     1     5
+    3     1     3
+    5     1     1
+*/
 class Solution {
     public List<TreeNode> allPossibleFBT(int n) {
         
