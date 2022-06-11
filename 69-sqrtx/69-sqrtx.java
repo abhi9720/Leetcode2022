1d9c13f34015bcc25b86a2e47ea117797cc51fff
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-11 10:48:32 +0530

    Time: 3 ms (29.08%), Space: 41.8 MB (22.13%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -3,7 +3,7 @@ class Solution {
         // binary search
         
         double  i = 1, j =  x;
-        while(j-i >= 1e-6){
+        while(j-i >= 1e-5){
             double mid =  (j-i)/2+i;
             if(mid*mid > x ){
                 j =  mid;
