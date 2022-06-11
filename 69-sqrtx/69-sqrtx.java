f37d45c5f3bbb4d88d5ea5d304f24c8c7686371f
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-11 10:47:55 +0530

    Time: 3 ms (29.08%), Space: 41.7 MB (22.13%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        // binary search
+        
+        double  i = 1, j =  x;
+        while(j-i >= 1e-6){
+            double mid =  (j-i)/2+i;
+            if(mid*mid > x ){
+                j =  mid;
+            }
+            else{
+                i =  mid;
+            }
+            
+        }
+        
+        return (int)j;
+    }
+}
