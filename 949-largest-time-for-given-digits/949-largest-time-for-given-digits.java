87be01593d14d39ed707e6bcc29cd0cd41386389
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-13 19:16:26 +0530

    Time: 21 ms (19.50%), Space: 43.3 MB (13.31%) - LeetHub

--- 949-largest-time-for-given-digits/949-largest-time-for-given-digits.java
+++ 949-largest-time-for-given-digits/949-largest-time-for-given-digits.java
@@ -1,16 +1,23 @@
 class Solution {
-
-    public String largestTimeFromDigits(int[] A) {
+    public String largestTimeFromDigits(int[] arr) {
         String ans = "";
-        for (int i = 0; i < 4; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                for (int k = 0; k < 4; ++k) {
-                    if (i == j || i == k || j == k) continue; // avoid duplicate among i, j & k.
-                    String h = "" + A[i] + A[j], m = "" + A[k] + A[6 - i - j - k], t = h + ":" + m; // hour, minutes, & time.
-                    if (h.compareTo("24") < 0 && m.compareTo("60") < 0 && ans.compareTo(t) < 0) ans = t; // hour < 24; minute < 60; update result.
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                for(int k =0;k<4;k++){
+                    if(i==j || j==k || k==i) continue;
+                    int l =  6-i-j-k;
+                    String HH  =  ""+arr[i]+arr[j];
+                    String MM =  ""+arr[k]+arr[l];
+                    String newTime =  HH+":"+MM;
+                    if(HH.compareTo("24") <0 && MM.compareTo("60") <0 ){
+                        if(newTime.compareTo(ans) >0 ){
+                            ans =  newTime;
+                        }
+                    }
                 }
             }
         }
         return ans;
+            
     }
 }
