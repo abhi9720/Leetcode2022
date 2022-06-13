577789313a3c7449947ed1e11cddcf9e3635a15e
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-13 23:00:43 +0530

    Time: 17 ms (12.27%), Space: 50.1 MB (93.02%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -6,8 +6,8 @@ class Solution {
         int m = grid[0].length;
         boolean visited[][] = new boolean[n][m];
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (int i = 1; i < n-1; i++) {
+            for (int j = 1; j < m-1; j++) {
                 if (i == 0 || j == 0) continue;
                 if (grid[i][j] == 1 && !visited[i][j]) {
                     
