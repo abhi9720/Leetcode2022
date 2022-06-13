98b8b3b8c5cb462486a007d6a7bc5944370d6407
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-14 00:50:31 +0530

    Time: 1 ms (92.10%), Space: 43.3 MB (6.90%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
@@ -1,21 +1,15 @@
 
 class Solution {
-    public boolean isCompleteTree(TreeNode root) {
-        Queue<TreeNode> q =  new LinkedList<>();;
-        q.add(root);        
-        boolean noChildFalg =  false;
-        while(q.size()>0){
-            TreeNode peek =  q.remove();
-           if(peek==null){
-               noChildFalg =  true;
-               continue;
-           }                                    
-            if(noChildFalg) return false;
-            q.add(peek.left);                        
-            q.add(peek.right);                        
-            
+  public boolean isCompleteTree(TreeNode root) {
+        Queue<TreeNode> bfs = new LinkedList<TreeNode>();
+        bfs.offer(root);
+        while (bfs.peek() != null) {
+            TreeNode node = bfs.poll();
+            bfs.offer(node.left);
+            bfs.offer(node.right);
         }
-        
-        return true;
+        while (!bfs.isEmpty() && bfs.peek() == null)
+            bfs.poll();
+        return bfs.isEmpty();
     }
 }
