f5ac2297ddf4b32ed17877cb582f8b88081a9c7a
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-14 22:10:28 +0530

    Time: 1 ms (81.80%), Space: 53.7 MB (47.66%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -5,15 +5,28 @@ class Solution {
         
         
         Integer visited[] = new Integer[vtx];
-        for (int i = 0; i < vtx; i++) {
-            if (visited[i] == null && bfs(graph, visited, i) == false) return false;
-        }
-
+        // for (int i = 0; i < vtx; i++) {
+        //     if (visited[i] == null && bfs(graph, visited, i) == false) return false;
+        // }        
+        // return true;
         
-
+        for(int i=0;i<vtx;i++){
+            if(visited[i]==null && !dfs(graph,1,i,visited)) return false;            
+        }
         return true;
     }
 
+    
+    private boolean dfs(int graph[][],int color ,int vtx, Integer visited[]){
+        if(visited[vtx]!=null){
+            return visited[vtx] == color;
+        }
+        visited[vtx] =  color;
+        for(int nbr : graph[vtx]){
+            if(!dfs(graph,-color,nbr,visited)) return false;
+        }
+        return true;
+    }
     private boolean bfs(int graph[][],Integer visited[],int vtx) {
         Queue<int[]> q = new LinkedList<>();
         
@@ -21,11 +34,11 @@ class Solution {
         while (q.size() > 0) {
             int peek[] = q.remove();
             if (visited[peek[0]] != null) {
+                // if color not matches 
                 if (visited[peek[0]] != peek[1]) return false;
                 continue;
             }
             visited[peek[0]] = peek[1];
-            // itertate over nbr            
             for (int nbr : graph[peek[0]]) {                
                 q.add(new int[] { nbr, (peek[1] + 1) % 2 });
                 
