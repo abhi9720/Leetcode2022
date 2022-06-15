bd8456f643ce6c28c047da09ed78c87c00cf86b8
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-15 23:11:22 +0530

    Time: 41 ms (14.59%), Space: 47.2 MB (26.22%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.java
+++ 778-swim-in-rising-water/778-swim-in-rising-water.java
@@ -1,37 +1,26 @@
 class Solution {
+
     int dir[][] =  {{-1,0},{1,0},{0,1},{0,-1}};
     public int swimInWater(int[][] grid) {
-        int n =  grid.length;
-        PriorityQueue<int[]> q =  new PriorityQueue<>((o1,o2)->o1[2]-o2[2]  );
-        boolean visited[][] =  new boolean[n][n];
-        q.offer(new int[]{0,0,grid[0][0]});// x,y,time
-        
-        while(q.size()>0 ){
-            int peek[] =  q.remove();
-            int x =  peek[0],y =  peek[1],t = peek[2];
-            if(visited[x][y]) continue;            
-            if(x==n-1 && y==n-1){
-                return t;
-            }
-            visited[x][y]  =  true;                        
-            for(int d[]:dir){
-                int nx  =  x+d[0] , ny =  y+d[1];
-                if(nx>=0 && ny>=0 && nx<n && ny<n && !visited[nx][ny] ){
-                    
-/* 
-Always insert neighbour, don't wait to raise our level
-we can only move through neighbour right?
-two cases -  our level less than neighbour or neighbour level less than our level
-in both cases, we have to wait for the same amount of time, so their level become equal
-and neighbour level will be Max of both currentNode level and neighbour level, at least one of them has to
-raise to the same level so the wait will be the max of both of their level.
- */
-                        q.offer(new int[]{nx,ny,Math.max(grid[nx][ny],t) }); 
-                    
-                }                
-            }                        
-            
+        int n = grid.length, m = grid[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b)->a[2]-b[2] );
+        pq.offer(new int[] { 0, 0, grid[0][0] });
+        boolean visited[][] =  new boolean[n][m];
+        int time = grid[0][0];
+        while (pq.size() > 0) {
+            int p[] = pq.remove();
+            int x = p[0], y = p[1], t = p[2];                        
+            time  =  Math.max(t,time);
+            if(x==n-1 && y==m-1) break;
+            if(visited[x][y]) continue;
+            visited[x][y] =  true;
+            for(int d[] : dir){
+                int nx =  x+d[0] , ny =  y+d[1];
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
+                    pq.offer( new int[]{nx,ny,grid[nx][ny]} );
+                }
+            }            
         }
-        return -1;
+        return time;
     }
 }
