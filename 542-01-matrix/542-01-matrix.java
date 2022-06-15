f3dbbfef84d818edd52c012e6840e63e441956e3
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-15 22:06:35 +0530

    Time: 8 ms (90.69%), Space: 73.6 MB (27.47%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,6 +1,18 @@
+/*
+
+for [i][j] -  we need to see all for its nbr 
+but for i,j all four nbr are not present at same time 
+so compuete i,j form top-left  nbr 
+then we compute i,j from bottom-right nbr 
+
+
+
+*/
+
 class Solution { // 5 ms, faster than 99.66%
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length, n = mat[0].length, INF = m + n; // The distance of cells is up to (M+N)
+        // top-left nbr 
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 if (mat[r][c] == 0) continue;
@@ -10,6 +22,7 @@ class Solution { // 5 ms, faster than 99.66%
                 mat[r][c] = Math.min(top, left) + 1;
             }
         }
+        // bottom-right nbr 
         for (int r = m - 1; r >= 0; r--) {
             for (int c = n - 1; c >= 0; c--) {
                 if (mat[r][c] == 0) continue;
