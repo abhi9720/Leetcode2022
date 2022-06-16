7c01f0aecc26d678906a627ea2bd67d59a55d8eb
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-16 11:45:50 +0530

    Time: 1 ms (22.72%), Space: 42.4 MB (8.41%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,35 +1,34 @@
 class MyStack {
+    Queue<Integer> q1 , q2;
 
-    Queue<Integer> q1 ,q2;
-    int size = 0;
     public MyStack() {
         q1 =  new ArrayDeque<>();
         q2 =  new ArrayDeque<>();        
     }
     
     public void push(int x) {
-        while(!q1.isEmpty ()){
-            q2.offer(q1.poll());
-        }
-        q1.offer(x);
-        while(!q2.isEmpty() ){
-            q1.offer(q2.poll());
-        }
+        while(!q1.isEmpty() ){
+			q2.offer(q1.poll());			
+		}
+		q1.offer(x);
+		while(!q2.isEmpty() ){
+			q1.offer(q2.poll());			
+		}
+
     }
     
     public int pop() {
-        if(q1.size()!=0 ) return q1.remove();
-        else return -1;
-        
+		if(empty()) return -1;
+        return q1.poll();
     }
     
     public int top() {
-        if(q1.size()!=0 ) return q1.peek();
-        else return -1;
+		if(empty()) return -1;
+        return q1.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+        return (q1.size() + q2.size()) == 0 ;
     }
 }
 
