ca6086e5697eb47b4abd3d8b1232b0d294db02d4
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-16 22:00:20 +0530

    Time: 1 ms (97.51%), Space: 54.9 MB (7.92%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -1,52 +1,32 @@
 class Solution {
-    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
-        int n  =  grid.length,m = grid[0].length;            
-        int ans[][] = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans[i][j] =  grid[i][j];
-            }
-        }
-        // start doing bfs from point of orgin 
-        Queue<int[]> q  =  new ArrayDeque<>();
-        q.offer(new int[]{row,col});
-        
-        
-        boolean visited[][] =  new boolean[n][m];
-        int cc  =  grid[row][col];
-        int dirs[][] =  {{-1,0},{0,1},{0,-1},{1,0}};
-        while(q.size()!=0 ){
-            int p[] =  q.remove();
-            if(visited[p[0]][p[1]] ) continue;
-            visited[p[0]][p[1]] =  true;
-            if(isBorder(grid,p[0],p[1],grid[p[0]][p[1]])){                                
-                ans[p[0]][p[1]] =  color;
-            }
-            for(int d[] : dirs){
-                int x =  d[0]+p[0];
-                int y =  d[1]+p[1];;                
-                if(x>=0 && y>=0 && x<n && y<m && grid[x][y]==cc && !visited[x][y]){
-                    q.offer(new int[]{x,y});
-                }
-            }                                    
-        }
-        
-        return ans;
-        
+
+    public int[][] colorBorder(int[][] grid, int r0, int c0, int color) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        if (grid[r0][c0] == color) return grid;
+
+        boolean[][] visited = new boolean[n][m];
+        dfs(grid, r0, c0, grid[r0][c0], visited, color, n, m);
+
+        return grid;
     }
-    
-    private boolean isBorder(int grid[][], int i, int j , int ele){
-        int n  =  grid.length,m = grid[0].length;            
-        // check left 
-        if( j-1<0 || (j-1>=0 && grid[i][j-1]!=ele)  ) return true;        
-        // check right
-        if( j+1==m || (j+1<m && grid[i][j+1]!=ele)  ) return true;
-        // check top
-        if( i-1<0 || (i-1>=0 && grid[i-1][j]!=ele) ) return true;
-        // check bottom
-        if( i+1==n || (i+1<n && grid[i+1][j]!=ele) ) return true;
-        
-        return false;
+
+    public void dfs(int[][] grid, int i, int j, int col, boolean[][] visited, int target, int n, int m) {
+        if (i > n - 1 || i < 0 || j > m - 1 || j < 0 || grid[i][j] != col || visited[i][j]) return;
+
+        visited[i][j] = true;
+        boolean border = false;
+
+        if (i == 0 || j == 0 || j == m - 1 || i == n - 1 
+            || grid[i + 1][j] != col || grid[i - 1][j] != col || 
+            grid[i][j - 1] != col || grid[i][j + 1] != col) border = true;
+
+        dfs(grid, i + 1, j, col, visited, target, n, m);
+        dfs(grid, i - 1, j, col, visited, target, n, m);
+        dfs(grid, i, j + 1, col, visited, target, n, m);
+        dfs(grid, i, j - 1, col, visited, target, n, m);
+
+        if (border) grid[i][j] = target;
     }
-    
 }
