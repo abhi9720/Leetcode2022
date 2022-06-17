01706bb0716eb09823bc6c3cbbc25c6ca7c50a3b
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-17 10:56:20 +0530

    Time: 10 ms (58.64%), Space: 45.1 MB (34.71%) - LeetHub

---
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -0,0 +1,33 @@
+class SortbyValue implements Comparator<int[]> {
+ public int compare(int a[], int b[]){
+        return a[0]-b[0];
+    }
+}
+
+class SortbyIndex implements Comparator<int[]> {
+ public int compare(int a[], int b[]){
+        return a[1]-b[1];
+    }
+}
+
+class Solution {
+    public int[] maxSubsequence(int[] nums, int k) {
+        PriorityQueue<int[]> pq =  new PriorityQueue<>(new SortbyValue());
+        for(int i=0;i<nums.length;i++){
+            pq.offer(new int[]{nums[i],i});
+            if(pq.size() > k ){
+                pq.poll();
+            }
+        }
+        
+        PriorityQueue<int[]> p =  new PriorityQueue<>( new SortbyIndex());        
+        while(!pq.isEmpty() ){
+            p.offer(pq.remove());
+        }
+        int seq[] =  new int[k];        
+        for(int i=0;i<k;i++){            
+            seq[i] = p.poll()[0];        
+        }
+        return seq;
+    }
+}
