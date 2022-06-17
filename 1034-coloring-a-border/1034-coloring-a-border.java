8be3df83e6895e22368b96790ab1802567e7e30e
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-17 16:46:52 +0530

    Time: 2 ms (43.69%), Space: 42.9 MB (95.50%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -1,44 +1,77 @@
 class Solution {
-
-    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
-        boolean[][] visit = new boolean[grid.length][grid[0].length];
-        help(grid, row, col,grid[row][col], color, visit);
+    public class Pair{
+        int i;
+        int j;
+        boolean border;
         
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] < 0) grid[i][j] = color;
+        Pair(int i,int j){
+            this.i=i;
+            this.j=j;
+        }
+    }
+    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        
+        ArrayDeque<Pair> q=new ArrayDeque<>();
+        boolean[][] visited=new boolean[grid.length][grid[0].length];
+        ArrayList<Pair> al=new ArrayList<>();
+        
+        Pair p=new Pair(row,col);
+        p.border=isBorder(grid,row,col);
+        q.add(p);
+        int oclr=grid[row][col];
+        while(q.size()>0){
+            Pair rem=q.remove();
+            if(visited[rem.i][rem.j]) continue;
+            visited[rem.i][rem.j]=true;
+            
+            al.add(rem);
+            
+           addNbr(rem.i+1,rem.j,grid,visited,q,oclr);
+           addNbr(rem.i-1,rem.j,grid,visited,q,oclr);
+           addNbr(rem.i,rem.j+1,grid,visited,q,oclr);
+           addNbr(rem.i,rem.j-1,grid,visited,q,oclr);
+        }
+        
+        for(Pair pp:al){
+            if(pp.border==true){
+                grid[pp.i][pp.j]=color;
             }
         }
-        
         return grid;
     }
-
-    public void help(int[][] grid, int row, int col,int selfcolor, int color, boolean[][] visit) {
-       if(row<0 || col<0 || row>=grid.length || col>=grid[0].length|| visit[row][col]
-          || grid[row][col]==0
-          || grid[row][col]!=selfcolor
-         ){
-           return;
-       }
-        
-        grid[row][col] = -grid[row][col];
-        visit[row][col] = true;
-        
-        
-        help(grid, row - 1, col,selfcolor, color, visit);
-        help(grid, row, col - 1,selfcolor, color, visit);
-        help(grid, row + 1, col,selfcolor, color, visit);
-        help(grid, row, col + 1,selfcolor, color, visit);
-        
-        if (
-            row>0 && col>0 && row<grid.length-1 && col<grid[0].length-1 &&
-            Math.abs(grid[row - 1][col]) == selfcolor &&
-            Math.abs(grid[row + 1][col]) == selfcolor &&
-            Math.abs(grid[row][col + 1]) == selfcolor &&
-            Math.abs(grid[row][col - 1]) == selfcolor
-        ) {
-            grid[row][col] = -grid[row][col] ;
-        }
+    
+    void addNbr(int i,int j,int[][] grid,boolean[][] visited,ArrayDeque<Pair> q,int oclr){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!=oclr || visited[i][j]==true)
+            return;
+        Pair np=new Pair(i,j);
+        np.border=isBorder(grid,i,j);
+        q.add(np);
     }
-     
+    
+    public boolean isBorder(int[][] grid,int i,int j){
+        if(i==0){
+            return true;
+        }
+        else if(j==0){
+            return true;
+        }
+        else if(i==grid.length-1)
+            return true;
+        else if(j==grid[0].length-1)
+            return true;
+        else {
+            int color=grid[i][j];
+            if(grid[i+1][j]!=color)
+                return true;
+            else if(grid[i][j+1]!=color)
+                return true;
+            else if(grid[i-1][j]!=color)
+                return true;
+            else if(grid[i][j-1]!=color)
+                return true;
+            else
+                return false;
+        }
+        
+    }
 }
