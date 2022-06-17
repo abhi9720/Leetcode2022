fc861ec37d9840e78a2881fe18a3f8a239da98a0
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-17 12:30:35 +0530

    Time: 7 ms (79.98%), Space: 47.1 MB (15.84%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    public int[] sortByBits(int[] arr) {        
+        mergeSort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private void mergeSort(int arr[], int i, int j) {
+        if (i == j) return;
+        int mid = (j - i) / 2 + i;
+        mergeSort(arr, i, mid);
+        mergeSort(arr, mid + 1, j);
+        mergeTwoSortedArray(arr, i, mid, j);
+    }
+
+    private void mergeTwoSortedArray(int arr[], int i, int mid, int j) {
+        
+        int sort[] =  new int[j-i+1];
+        int p1 = i, p2 = mid + 1;
+        // System.out.println(p1+" - "+(mid) +"  | "+ p2+" - "+j);
+        // System.out.println(Arrays.toString(ans) );
+        int itr = 0;
+        while (p1 <= mid && p2 <= j) {
+                            
+            if ( compareTo(arr[p1], arr[p2]) < 0) {                
+                sort[itr++] = arr[p1++];                
+            } else {
+                sort[itr++] = arr[p2++];                
+            }
+        }
+        while(p1<=mid) sort[itr++] = arr[p1++];     
+        while(p2<=j) sort[itr++] = arr[p2++];
+        System.arraycopy(sort,0,arr,i,sort.length);
+        
+    }
+
+//     public int bitCount(Integer n) {
+//         int bit = 0;
+//         while (n != 0) {
+//             int rsb = n & -n;
+//             n = n - rsb;
+//             bit++;
+//         }
+
+//         return bit;
+//     }
+
+    public int compareTo(int a, int b) {
+        int diff = Integer.bitCount(a) - Integer.bitCount(b);
+        if(diff==0) return Integer.compare(a,b);
+        else return diff;
+    }
+}
