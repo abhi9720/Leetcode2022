8749a30d07189c7a6c623c0b7c7ff00877f582ae
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-17 19:27:15 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (87.28%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,27 +1,29 @@
-
 class Solution {
-    static final int MONITER = 1;
-    static final int NON_MONITER = 2;
-    static final int CAMERA = 3;
+    final int CAMERA = 1;
+    final int MONITORED = 2;
+    final int NON_MONITORED = 3;
     int count = 0;
 
     public int minCameraCover(TreeNode root) {
-        int rootStatus = cameraHelper(root);
-        return rootStatus == NON_MONITER ? count + 1 : count;
+        this.count = 0;
+        int rootStatus = helper(root);
+        return rootStatus == NON_MONITORED? count+1:count;
     }
 
-    private int cameraHelper(TreeNode root) {
-        if (root == null) return MONITER;
-
-        int left = cameraHelper(root.left);
-        int right = cameraHelper(root.right);
-
-        if (left == NON_MONITER || right == NON_MONITER) {
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return MONITORED;
+        }
+        
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if (left == NON_MONITORED || right == NON_MONITORED) {
             count += 1;
             return CAMERA;
-        } else if (left == CAMERA || right == CAMERA) return MONITER;
-		 else {
-            return NON_MONITER;
+        } else if (left == CAMERA || right == CAMERA) {
+            return MONITORED;
+        } else {
+            return NON_MONITORED;
         }
     }
 }
