dc198bbd8a1916abdc67230b3c37c9afec538568
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-18 18:53:26 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.java
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -32,53 +32,38 @@ class GFG{
 class Solution
 { 
     //Function to find if there is a celebrity in the party or not.
-    int celebrity(int M[][], int n)
-    {
-        Stack<Integer> stk  =  new Stack<>();
-        
-        for(int i=0;i<n;i++ ){
-            stk.push(i);
+    int celebrity(int M[][], int n){      
+      Stack<Integer> stk =  new Stack<>();
+      for(int i=0;i<n;i++){
+        stk.push(i);
+      }
+      int count = 0;
+      while(stk.size()>1 ){
+        int p1 =  stk.pop();
+        int p2 =  stk.pop();
+        // if p1 knows p2 than p1 cannot be celebrity
+        // if p2 knows p1 than  p2 cannot be celebrity
+        // if p1 and p2 don't know any each other p1 cannot be celebrity 
+        // as celebrity is know by every one 
+
+        if(M[p1][p2]==1){
+          stk.push(p2);
         }
-        
-        int count = 0;
-        while(stk.size()>1 ){
-            int p1  =  stk.pop(), p2 =  stk.pop();
-            if(M[p1][p2]==1){
-                stk.push(p2);
-            }
-            else{
-                stk.push(p1);
-                }
-                if(M[p1][p2]==0 && M[p2][p1]==0){
-                    count++;
-                }
-            }
-            int p =  stk.peek();
-            for(int i=0;i<n;i++){
-                if(M[p][i]!=0) return -1;
-            }
-            if(count==n-1) return -1;
-    
-        return p;
-        
-        // n^2
-        // int celebrity =  -1;
-        // for(int i=0;i<n;i++){
-        //     int count= 0;
-        //     for(int mapping : M[i]){
-        //         if(mapping==0){
-        //             count+=1;
-        //         }
-        //     }
-        //     if(count==n){
-        //         if(celebrity!=-1){
-        //             return -1;
-        //         }
-        //         celebrity =  i;
-        //     }
-        // }
-        
-        // return celebrity;
-    	
+        else  {
+          stk.push(p1);
+          if(M[p1][p2]==0 && M[p2][p1]==0){
+                count++;
+          }
+        }
+      }
+
+      // atlast make sure reamined person is celebrity 
+      int p =  stk.peek();
+      for(int i=0;i<n;i++){
+        if(M[p][i]==1) return -1;
+      }
+      
+      if(count==n-1) return -1;
+      return p;    	 
     }
 }
