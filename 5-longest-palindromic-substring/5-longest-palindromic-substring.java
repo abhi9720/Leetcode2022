3d34a0df6e0a115a52eadfcfc0f6b1ed40e098b4
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-18 23:17:26 +0530

    Time: 392 ms (26.45%), Space: 80.8 MB (24.93%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,48 +1,25 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
- //Approch 1        
-//         int n =  s.length();
-//         boolean dp[][] = new boolean[n][n];
-//         String ans = "";
-//         for(int gap=0;gap<n;gap++){            
-//             for(int i=0, j=gap ; j<n ; i++,j++){
-//                 if(gap==0) dp[i][j] = true;
-//                 else if(gap==1) dp[i][j] =  s.charAt(i)==s.charAt(j);
-//                 else{                    
-//                     dp[i][j] =  dp[i+1][j-1]  && ( s.charAt(i)==s.charAt(j));                                                        }                
-//                 if(gap+1 > ans.length() && dp[i][j] ){
-//                     ans  = s.substring(i,j+1);
-//                 }
-//             }            
-//         }
-//         return ans;
-        
-        // Approch 2
-        
-        // taking each center as palindrome center and explore about each center 
-        
-        if(s==null || s.length() ==0) return "";
-        int n =  s.length();
-        int start = 0, end=0;
-        for(int i=0;i<n;i++){
-            int len1 = expandCenter(s,i,i);
-            int len2 = expandCenter(s,i,i+1);
-            int len = Math.max(len1,len2);
-            if(len>end-start){
-                start =  i-(len-1) / 2; //  -1 beacuse of even length , they have len/2-1 element on left 
-                end =  i+(len)/2; // they have len/2 element on rigt 
-            }            
+        int n  =  s.length();
+        boolean dp[][] =  new boolean[n][n];
+        int st = 0 ,len = 0;
+        for(int gap=0;gap<n;gap++){
+            for(int i=0,j =  gap; i+gap<n; i++ , j++  ){
+                if(gap==0){
+                    dp[i][j] =  true;
+                }
+                else if(gap==1){
+                    dp[i][j] =  s.charAt(i) == s.charAt(j);
+                }
+                else{
+                    dp[i][j] =  dp[i+1][j-1] && (s.charAt(i) == s.charAt(j));
+                }       
+                if(dp[i][j] && (j-i+1) > len){
+                    len  =  j-i+1;
+                    st  = i;
+                }
+            }
         }
-        return s.substring(start,end+1);                        
-    }
-    
-    private int expandCenter(String s , int l , int r){
-        
-        while(l>=0 && r<s.length()  && s.charAt(l)==s.charAt(r)){
-            l--;r++;
-        }
-        return r-l-1;
-        
+        return s.substring(st,st+len);                
     }
 }
