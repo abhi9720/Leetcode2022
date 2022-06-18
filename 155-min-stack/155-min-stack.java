4ac523c65040978fa39aee7cabe945f362c1aafe
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-18 13:14:52 +0530

    Time: 5 ms (87.19%), Space: 43.8 MB (98.25%) - LeetHub

---
+++ 155-min-stack/155-min-stack.java
@@ -0,0 +1,34 @@
+class MinStack {
+    Stack<Integer> data , min;
+    public MinStack() {
+        data  =  new Stack<>();
+        min =  new Stack<>();
+    }
+    
+    public void push(int val) {
+        data.push(val);
+		if(min.size()==0 || min.peek() >= val ){
+			min.push(val);
+		}
+        
+    }
+    
+    public void pop() {
+        int popped =  data.pop();
+		if( popped ==  min.peek()){            
+			min.pop();
+		}		
+        
+    }
+    
+    public int top() {
+		if(data.size()==0) return -1;
+        return data.peek();
+    }
+    
+    public int getMin() {
+        if(min.size() ==0 ) return -1;
+		return min.peek();
+    }
+}
+
