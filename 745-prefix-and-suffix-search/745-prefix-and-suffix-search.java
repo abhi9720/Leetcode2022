4e664d1208549320c7e38a8fe6dc0a12309e76a9
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-18 18:35:17 +0530

    Time: 551 ms (37.32%), Space: 256.5 MB (39.55%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
@@ -1,3 +1,13 @@
+/*
+
+Consider the word 'apple'. For each suffix of the word, we could insert that suffix, followed by '#', followed by the word, all into the trie.
+
+For example, we will insert '#apple', 'e#apple', 'le#apple', 'ple#apple', 'pple#apple', 'apple#apple' into the trie. Then for a query like prefix = "ap", suffix = "le", 
+we can find it by querying our trie for le#ap.
+
+*/
+
+// here wt is indexing 
 class WordFilter {
     TrieNode trie;
     public WordFilter(String[] words) {
