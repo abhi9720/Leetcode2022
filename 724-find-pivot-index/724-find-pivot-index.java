b33c7be8e456524f7f9c02a4b30f86d16eca7c05
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-19 13:33:41 +0530

    Time: 1 ms (99.98%), Space: 43.1 MB (87.49%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -6,12 +6,11 @@ class Solution {
         }   
         int l = 0, r = sum-nums[0];
         if(l==r) return 0;
-        for(int i=0;i<nums.length-1;i++){
-            if(l==r) return i;
+        for(int i=0;i<nums.length;i++){
+            if(l==r) return i;            
             l+=nums[i];
-            r-=nums[i+1];
-        }
-        if(l==r) return nums.length-1;
+            if(i+1<nums.length) r-=nums[i+1];
+        }        
         return -1;
     }
 }
