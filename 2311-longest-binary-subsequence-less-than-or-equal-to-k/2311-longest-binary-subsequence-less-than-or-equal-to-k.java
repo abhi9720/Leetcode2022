5e296a67ed1622b20b168b8b4d7dec6ad049d97d
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-19 11:13:27 +0530

    Time: 9 ms (66.67%), Space: 42.2 MB (50.00%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.java
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.java
@@ -1,21 +1,23 @@
 class Solution {
 
     public int longestSubsequence(String s, int k) {
-        int n  =  s.length();
-        int val = 0, cnt = 0, pow = 1;
-        cnt = (int)s.chars().filter(ch -> ch == '0').count();
-        for(int i=n-1;i>=0 && val +pow <=k ;i-- ){
-            if(s.charAt(i)=='1' ){
-                val+=pow;                
+        int n = s.length();
+        int j = n - 1;
+        int len = 0;
+        int val = 0 , pw =  1;
+        len = (int) s.chars().filter(ch -> ch == '0').count();
+        while (j >= 0 && val + pw <= k) {
+            if (s.charAt(j) == '0') {
+                len-=1;
+            } else{
+                val += pw;                
             }
-            else{
-                cnt--;
-            }
-            cnt++;
-            pow = pow*2 ;
+            
+            len+=1;
+            pw = pw * 2;
+            j -= 1;
         }
-        return cnt;
 
-        
+        return len;
     }
 }
