ef6b2be133374baae3f866711ea268e1cd6763e2
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-19 15:18:30 +0530

    Time: 3 ms (34.27%), Space: 41.5 MB (77.61%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,26 +1,22 @@
 class Solution {
-
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        return longestCommonPrefix(strs, 0, strs.length - 1);
-    }
-
-    private String longestCommonPrefix(String[] strs, int l, int r) {
-        if (l == r) {
-            return strs[l];
-        } else {
-            int mid = (l + r) / 2;
-            String lcpLeft = longestCommonPrefix(strs, l, mid);
-            String lcpRight = longestCommonPrefix(strs, mid + 1, r);
-            return commonPrefix(lcpLeft, lcpRight);
+        Arrays.sort(strs , (s1,s2)->{
+            return s1.length() - s2.length();
+        });
+        String prefix =  strs[0];
+        int n =  strs.length;
+        
+        for(int i=1;i<n;i++){
+            // compare prefix and strs[i] from start
+            int j=0;
+            String str =  strs[i];
+            while(j<Math.min(prefix.length() , str.length() ) ){
+                if(prefix.charAt(j) != str.charAt(j) ) break;
+                j++;
+            }
+            prefix = prefix.substring(0,j);                        
+            if(prefix.length()==0 ) break;
         }
-    }
-
-    String commonPrefix(String left, String right) {
-        int min = Math.min(left.length(), right.length());
-        for (int i = 0; i < min; i++) {
-            if (left.charAt(i) != right.charAt(i)) return left.substring(0, i);
-        }
-        return left.substring(0, min);
+        return prefix;
     }
 }
