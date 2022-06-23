10e0dd6d7dbba032e1b9ba5d2aabd4f60bc0c9dd
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-23 22:12:03 +0530

    Time: 59 ms (49.81%), Space: 68.6 MB (66.85%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,15 +1,21 @@
-public class Solution {
+class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses,(a,b)->a[1]-b[1]); //Sort the courses by their deadlines (Greedy! We have to deal with courses with early deadlines first)
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
-        int time=0;
-        for (int[] c:courses) 
-        {
-            time+=c[0]; // add current course to a priority queue
-            pq.add(c[0]);
-            if (time>c[1]) time-=pq.poll(); 
-            //If time exceeds, drop the previous course which costs the most time. (That must be the best choice!)
-        }        
-        return pq.size();
+        // try to compelete those courses first which have smaller deadline 
+        Arrays.sort(courses , (a,b)-> a[1] - b[1]);
+        PriorityQueue<Integer> pqmax  =  new PriorityQueue<>( (a,b)-> b-a );
+        int time = 0;
+        for(int c [] : courses){
+            time+=c[0];
+            pqmax.add(c[0]);
+            if(time > c[1] ){
+                // completing this course will cross will cross deadline 
+                // so we will remove the course which have maximum duration 
+                // including current course also
+                time-=pqmax.poll();                
+            }            
+        }
+        return pqmax.size();
+        
+        
     }
 }
