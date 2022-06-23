508a5684552d502e9c49a3bbdb51b27fee1e76dc
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-23 13:53:28 +0530

    Added solution - LeetHub

--- Left View of Binary Tree - GFG/left-view-of-binary-tree.java
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.java
@@ -124,25 +124,24 @@ class Tree
     //Function to return list containing elements of left view of binary tree.
     ArrayList<Integer> leftView(Node root)
     {
+        ArrayList<Integer> li =  new ArrayList<>();
+        leftViewDFS(root,0,li);
+        return li;
       // Your code here
-      Queue<Node> q =  new ArrayDeque<>();
-      ArrayList<Integer> li =  new ArrayList<>();
-      if(root==null) return li;
-      q.offer(root);
-      while(!q.isEmpty() ){
-          int size =  q.size();
-          
-          for(int i=0;i<size ;i++ ){
-              Node peek  =  q.poll();
-              if(i==0) li.add(peek.data);
-              if(peek.left!=null){
-                  q.offer(peek.left);
-              }
-              if(peek.right!=null){
-                  q.offer(peek.right);
-              }
-          }
-      }
-      return li;
     }
+    
+    private void leftViewDFS(Node root, int depth , ArrayList<Integer> path){
+        if(root==null) return ;
+        
+        // as we first time reach new depth , add its in list 
+        // as path size will be increased for other node at same depth
+        // they cannot be added them
+        if(depth == path.size() ){
+            path.add(root.data);
+        }
+        
+        leftViewDFS(root.left,depth+1,path);
+        leftViewDFS(root.right,depth+1,path);
+    }
+    
 }
