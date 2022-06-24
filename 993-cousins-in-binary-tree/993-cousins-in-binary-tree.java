d22c943abd5b0a9482c83a5b6ae8c3faf1bab2f9
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-24 12:32:05 +0530

    Time: 1 ms (62.72%), Space: 42.7 MB (8.00%) - LeetHub

---
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        int d1 = getDepth(root, x);
+        int d2 = getDepth(root, y);
+        if (d1 == d2) {
+            TreeNode lca = lowestCommonAncestor(root, x, y);
+
+            if (lca.val != x && lca.val != y && lca.left != null && lca.right != null) {
+                return !((lca.left.val == x && lca.right.val == y) 
+                         || (lca.left.val == y && lca.right.val == x));
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
+        if (root == null) return null;
+        if (root.val == p || root.val == q) return root;
+
+        TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);
+        if (leftLCA != null && rightLCA != null) return root; else if (leftLCA == null) return rightLCA; else return leftLCA;
+    }
+
+    private int getDepth(TreeNode node, int val) {
+        if (node == null) return -1;
+        if (node.val == val) return 0;
+        int lf = getDepth(node.left, val);
+        int rf = getDepth(node.right, val);
+        if (lf != -1) return lf + 1; else if (rf != -1) return rf + 1; else return -1;
+    }
+}
