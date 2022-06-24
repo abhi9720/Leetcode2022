d08a39c79d87ff5d0a1fd76dc8df70acdd8707e5
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-24 09:32:19 +0530

    Time: 1 ms (97.96%), Space: 44.1 MB (12.19%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
@@ -1,52 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        
-        List<List<Integer>>  ans = new ArrayList<>();
-        if(root==null) return ans;
-        ArrayList<Integer> level =  new ArrayList<>();
-        Stack<ArrayList<Integer>> stk =  new Stack<>();
-        
-        Queue<TreeNode> q =  new LinkedList<>();
-        q.add(root);
-        q.add(null);
-        while(q.size() > 1 ){
-            TreeNode peek =  q.remove();
-            
-            if(peek==null){
-                stk.add( level );
-                level =  new ArrayList<>();
-                q.add(null);
-                continue;
-            }
-            level.add(peek.val);
-            if(peek.left!=null){
-                q.add(peek.left);                
-            }
-            
-            if(peek.right!=null){
-                q.add(peek.right);
-            }            
-        }
-        stk.add( level );
-        while(stk.size() >0 ){
-            ans.add(stk.pop() );
+        List<List<Integer>> ans =  new ArrayList<>();
+        dfs(root,0,ans);
+        int size =  ans.size();
+        for(int i=0;i<size/2;i++){
+            int j =  size - 1 -i;
+            List<Integer> temp =  ans.get(i);
+            ans.set(i ,  ans.get(j) );
+            ans.set(j , temp);
         }
         return ans;
+    }
+    private void dfs(TreeNode root, int depth , List<List<Integer>> ans){
         
+        if(root==null) return ;
+        if( depth>= ans.size() ){
+            ans.add(new ArrayList<>());            
+        }
+        ans.get(depth).add(root.val);
+        dfs(root.left,depth+1,ans);
+        dfs(root.right,depth+1,ans);            
     }
 }
