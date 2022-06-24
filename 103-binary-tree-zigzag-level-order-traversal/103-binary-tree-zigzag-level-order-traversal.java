ca961946b2214d27f72b1d4aee07cd1e99dbc067
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-24 09:21:37 +0530

    Time: 2 ms (32.96%), Space: 42.7 MB (51.77%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -1,48 +1,42 @@
+
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Stack<TreeNode > stk =  new Stack<>();
-        Queue<TreeNode> q =  new ArrayDeque<>();
         List<List<Integer>> ans =  new ArrayList<>();
         if(root==null) return ans;
-        stk.add(root);
+        Stack<TreeNode> stk1 =  new Stack<>();
+        Stack<TreeNode> stk2 =  new Stack<>();
+        stk1.push(root);
         int level =  0;
-        // 0 level L to R
-        // 1 level R to L
-        while(stk.size()>0 ){
-            List<Integer> level_nodes =  new ArrayList<>();
-            
-            if(level==0){// l to r
-                while(stk.size() >0 ){
-                    TreeNode peek =  stk.pop();
-                    if(peek.left!=null){
-                        q.add(peek.left);
-                    }                    
-                    if(peek.right!=null){
-                        q.add(peek.right);
-                    }
-                    level_nodes.add(peek.val);
-                }                
-                level =  1;
+        
+        while(stk1.size()!=0 || stk2.size()!=0 ){    
+            ArrayList<Integer> le =  new ArrayList<>();
+            if(level%2==0){
+                // left child , right child 
+                while(stk1.size()!=0 ){
+                    TreeNode p =  stk1.pop();
+                    le.add(p.val);
+                    if(p.left!=null) stk2.push(p.left);
+                    if(p.right!=null) stk2.push(p.right);                
+                }
+                level++;
             }
             else{
-                 while(stk.size() >0 ){
-                    TreeNode peek =  stk.pop();
-                    if(peek.right!=null){
-                        q.add(peek.right);
-                    }
-                    if(peek.left!=null){
-                        q.add(peek.left);
-                    }                    
-                    level_nodes.add(peek.val);
-                } 
-                level =  0;
-            }
-            ans.add(level_nodes);
-            
-            stk.addAll(q);
-            q.clear();
-            
-        }
+                // right child  , left child 
+                while(stk2.size()!=0 ){
+                    TreeNode p =  stk2.pop();
+                    le.add(p.val);
+                    if(p.right!=null) stk1.push(p.right);                
+                    if(p.left!=null) stk1.push(p.left);                    
+                }
+                level++;
+            }   
+            ans.add(le);
+        }        
         return ans;
     }
 }
+
+
+    
+    
+ 
