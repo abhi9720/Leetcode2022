9a73f425567ef7f790075a1418615d1ee79f2dfd
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-25 21:48:02 +0530

    Time: 2 ms (54.32%), Space: 43 MB (88.88%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,51 +1,47 @@
 class Solution {
-
+    int parent[] , rank[];
     public boolean isBipartite(int[][] graph) {
-        int vtx = graph.length;
+        int vtx =  graph.length;
+        parent =    new int[vtx];
+        rank =  new int[vtx];
+        for(int i=0;i<parent.length;i++){
+            parent[i]  = i;
+        }
         
-        
-        Integer visited[] = new Integer[vtx];
-        // bfs
-        // for (int i = 0; i < vtx; i++) {
-        //     if (visited[i] == null && bfs(graph, visited, i) == false) return false;
-        // }        
-        // return true;
-        
-        // dfs
         for(int i=0;i<vtx;i++){
-            if(visited[i]==null && !dfs(graph,1,i,visited)) return false;            
+            for(int j=0;j<graph[i].length;j++){
+                if(find(i) == find(graph[i][j]) ){                    
+                    return false;
+                }
+                // else make use of this edge and connection them in union set
+                // donot do union of i with graph[i][j]
+                // as we nbr graph[i][j] check for its nbr and i will be parent in it 
+                // and they have parent as we union them before and its return false 
+                union(find(graph[i][0]),find(graph[i][j]));
+            }
         }
         return true;
     }
-
     
-    private boolean dfs(int graph[][],int color ,int vtx, Integer visited[]){
-        if(visited[vtx]!=null){
-            return visited[vtx] == color;
-        }
-        visited[vtx] =  color;
-        for(int nbr : graph[vtx]){
-            if(!dfs(graph,-color,nbr,visited)) return false;
-        }
-        return true;
+    private int find(int x){        
+        if(x!=parent[x]) parent[x] = find(parent[x]);
+        return parent[x];
     }
-    private boolean bfs(int graph[][],Integer visited[],int vtx) {
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[] { vtx, 0 });
-        while (q.size() > 0) {
-            int peek[] = q.remove();
-            if (visited[peek[0]] != null) {
-                // if color not matches 
-                if (visited[peek[0]] != peek[1]) return false;
-                continue;
-            }
-            visited[peek[0]] = peek[1];
-            for (int nbr : graph[peek[0]]) {                
-                q.add(new int[] { nbr, (peek[1] + 1) % 2 });
-                
-            }
+    
+    
+    // s1 and s2 lead 
+    private void union(int s1L , int s2L){
+        if(rank[s1L] < rank[s2L]){
+            parent[s1L] =  s2L;
         }
-        return true;
+        else if(rank[s2L] < rank[s1L]){
+            parent[s2L] =  s1L;
+        }
+        else{
+            parent[s1L] =  s2L;
+            rank[s2L]++;
+        }
+        
+        
     }
 }
