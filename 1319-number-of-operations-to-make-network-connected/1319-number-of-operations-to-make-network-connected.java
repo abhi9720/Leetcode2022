4eebf1c8c33ae16efe79282652f455d6a013dcb7
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-27 00:43:21 +0530

    Time: 10 ms (68.50%), Space: 66.5 MB (48.36%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,47 @@
+class Solution {
+    int parent[], rank[];
+
+    public int makeConnected(int n, int[][] connections) {
+        parent = new int[n];
+        rank = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i] =  i;
+        }
+        
+        int count = 0;
+        for(int ed[] : connections){
+            int s1L =  find(ed[0]);
+            int s2L =  find(ed[1]);
+            if(s1L==s2L){// redundant connection 
+                count+=1;
+            }
+            else{
+                // System.out.println(Arrays.toString(ed)+" "+s1L+" : "+s2L );
+                union(s1L , s2L);
+            }
+        }
+        // System.out.println(Arrays.toString(parent) );
+        HashSet<Integer> unique =  new HashSet<>();
+        for(int ele : parent){
+            unique.add(find(ele));
+        }
+        int wirerequired  = unique.size() - 1 ;
+        
+        return wirerequired > count ?-1 : wirerequired;
+        
+    }
+
+    private int find(int x) {
+        while (x != parent[x]) x = parent[x];
+        return parent[x];
+    }
+
+    private void union(int s1L, int s2L) {
+        if (rank[s1L] < rank[s2L]) parent[s1L] = s2L;
+        else if (rank[s1L] > rank[s2L]) parent[s2L] = s1L;
+        else {
+            parent[s1L] = s2L;
+            rank[s2L]++;
+        }
+    }
+}
