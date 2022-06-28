82ac8fec0f2af2d2553594e1ba689408e7c40f4c
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-29 01:00:09 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (43.14%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -2,19 +2,20 @@ class Solution {
 
     public int[] plusOne(int[] digits) {
         int c = 0, s = 0, n = digits.length;
-        int ans[] = new int[n];
+        
         for (int i = n - 1; i >= 0; i--) {
-            s = digits[i] + c + (i == n - 1 ? 1 : 0);
+            s = digits[i] + c + (i == n - 1 ? 1 : 0);            
             c = s / 10;
             s = s % 10;
-            ans[i] =  s;
+            digits[i] =  s;
+            if(digits[i]!=0) break;
         }
         if (c != 0) {
-            int temp[] =  ans;
-            ans=  new int[n+1];
-            ans[0] =  c;
-            System.arraycopy(temp,0,ans,1,n);
+            int temp[] =  digits;
+            digits=  new int[n+1];
+            digits[0] =  1;            
         }
-        return ans;
+        
+        return digits;
     }
 }
