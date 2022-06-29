496313a97b377fb68e5c8b67d1c52ece9865b053
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-29 17:42:50 +0530

    Time: 2 ms (9.51%), Space: 40.7 MB (57.63%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,19 +1,21 @@
+// This is a math problem：
+// 1 = 1
+// 4 = 1 + 3
+// 9 = 1 + 3 + 5
+// 16 = 1 + 3 + 5 + 7
+// 25 = 1 + 3 + 5 + 7 + 9
+// 36 = 1 + 3 + 5 + 7 + 9 + 11
+// ....
+// so 1+3+...+(2n-1) = (2n-1 + 1)n/2 = nn
+// if numbr is perfect square then it can be expressed like this 
+
 class Solution {
-    public boolean isPerfectSquare(int num) {
-        double i = 1 , j =  2147483647;
-        while(j-i >= 1e-1){
-            double mid =  (j-i)/2+i;
-            if(mid < num/mid ){
-                i =  mid+1;
-            }
-            else if( mid > num/mid ){
-                j  = mid-1;
-            }
-            else{
-                return true;
-            }            
+    public boolean isPerfectSquare(int n) {
+        int i = 1;
+        while(n>0){
+            n-=i;
+            i+=2;
         }
-        
-        return i*j==num;
+        return n==0;
     }
 }
