a94b104b7bef25d2d9dc4d2238fa5e6df03ef6e9
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-06-29 17:34:56 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (49.91%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        double i = 1 , j =  2147483647;
+        while(j-i >= 1e-7){
+            double mid =  (j-i)/2+i;
+            if(mid < num/mid ){
+                i =  mid+1;
+            }
+            else if( mid > num/mid ){
+                j  = mid-1;
+            }
+            else{
+                return true;
+
+            }            
+        }
+        
+        return i*j==num;
+    }
+}
