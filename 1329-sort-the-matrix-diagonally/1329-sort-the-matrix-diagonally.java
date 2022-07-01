e14e1f15db1099d3182264d93e6582812aa1d8bc
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-01 20:53:21 +0530

    Time: 13 ms (33.79%), Space: 47.7 MB (69.32%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int[][] diagonalSort(int[][] mat) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = mat.length, m = mat[0].length;
+
+        int i = 0, j = m - 1;
+        while (i != n - 1 || j != 0) {
+            for (int x = i, y = j; x < n && y < m; x++, y++) {
+                pq.offer(mat[x][y]);
+            }
+
+            for (int x = i, y = j; x < n && y < m; x++, y++) {
+                mat[x][y] = pq.poll();
+            }
+            if (j - 1 >= 0) j -= 1; else i++;
+        }
+        return mat;
+    }
+}
+/*
+Approach1 :  
+    1. Traverse diagonal , store in list , sort the list.    
+    2. loop diagonal again and fill with list element 
+    // or use PriorityQueue        
+*/
