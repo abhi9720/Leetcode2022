5534e873454e4d60752c9b848695e947e34f34c8
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-01 22:19:40 +0530

    Added solution - LeetHub

--- Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
@@ -38,28 +38,65 @@ class Solution
     //Function to find the minimum number of swaps required to sort the array.
     public int minSwaps(int nums[])
     {
+        int n =  nums.length;
+        parent =  new int[n];
+        size =  new int[n];
+        for(int i=0;i<n;i++){
+            parent[i] =  i;
+        }
+        Arrays.fill(size,1);
+        
         ArrayList<pair> arrpos =  new ArrayList<>();
+        
         for(int i=0;i<nums.length;i++){
             arrpos.add( new pair(nums[i] ,  i ) );
         }
         Collections.sort(arrpos ,  (a,b)-> a.ele  - b.ele );
         
-        boolean visit[] = new boolean[nums.length];
+        
         int swap = 0;
-        for(int i=0;i<nums.length;i++){
-            int j = i;
-            int len = 0;
-            // only run if not visited and only for those element which are not
-            // at its correct position
-            while(!visit[j] && arrpos.get(i).idx!=i){
-                visit[j] =  true;
-                len++;
-                j =  arrpos.get(j).idx;
+        for(int i=0;i<n;i++){
+            if(i!=arrpos.get(i).idx ){
+                int s1L  =  find(i);
+                int s2L  =  find(arrpos.get(i).idx) ;
+                if(s1L!=s2L){
+                    union(s1L ,s2L );
+                }
+                
             }
-            
-            swap+=Math.max(len-1 , 0);
         }
+        
+        boolean used[] =  new boolean[n];
+        for(int i=0;i<n;i++){
+            int lead = find(i);
+            if(!used[lead]){
+                used[lead] =  true;
+                swap+=size[lead]-1;
+            }
+        }
+   
         return swap;
         
     }
+    int parent[] , size[];
+    int find(int x){
+        if(parent[x] !=x ){
+            parent[x] =  find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    void union(int s1 , int s2){
+     if(size[s1] <= size[s2] ){
+         parent[s1] =  s2;
+         size[s2]+=size[s1];
+     }
+     else{
+         parent[s2] =  s1;
+         size[s1]+=size[s2];
+     }
+    }
+    
+    
+    
 }
