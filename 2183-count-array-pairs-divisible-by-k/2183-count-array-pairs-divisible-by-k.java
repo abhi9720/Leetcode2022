b1f1bc3fd17a1d67e939d22d043e3397e1c0a33d
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-02 12:10:14 +0530

    Time: 1093 ms (21.66%), Space: 95.2 MB (63.06%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
@@ -1,22 +1,38 @@
 /*
 
 
-If (a*b)%k==0, then gcd(a,k) * gcd(b,k) % k is also 0.
+
+If (a*b)%k==0, then gcd(a,k) * gcd(b,k) % k is also divible by k
+0.
 prove : 
-1. you can see this intuitively. When a number is divisible by another number, its prime factorization is a superset of the prime factorization of that number.
+1. you can see this intutively. When a number is divisible by another number, its prime factorization is a superset of the prime factorization of that number.
 
 For example, 5544 (2 * 2 * 2 * 3 * 3 * 7 * 11) is divisible by 84 (2 * 2 * 3 * 7).
 
-2. For example, GDC of 198 (2 * 3 * 3 * 11) and 84 (2 * 2 * 3 * 7) is 6 (2 * 3).
+2. For example, N1 = 198 (2 * 3 * 3 * 11) and N2 = 5544 (2 * 2 * 2 * 3 * 3 * 7 * 11)
+    k =  42(2 * 3 * 7)
+    gcd(198 , 42)  =  (2*3)
+    gcd(5544 , 42) = (2 * 3 * 7 )
+    => (2*3)*(2*3*7) =  2*2*3*3*7 superset of 42(2*3*7) so 42 divide it 
+    
+    198 (2 * 3 * 3 * 11) x 5544 (2 * 2 * 2 * 3 * 3 * 7 * 11) 
+    => (2 * 3 * 3 * 11) * (2 * 2 * 2 * 3 * 3 * 7 * 11)  superset of 42(2*3*7) so 42 divide it 
+    
+    
     With that, we can see that 
     
     gcd(n1, k) * gcd(n2, k)% k = n1 * n2 % k. 
     gcd =  maximum common factor 
+
+//---------------------------------------------------------------------------------------
    
     gcd(n1, k) - common between n1 and k
     gcd(n2, k) - and other left will be provided by n2 and k
+    and gcd(n1, k) * gcd(n2, k) will have all prime factor of k 
+    // so gcd(n1, k) * gcd(n2, k)%k==0
     
-    n1 * n2 % k = which is equal to n1*n2 % k
+    n1 * n2 % k ==0 - The product of these 2 numbers will be divisible by a number k 
+                    iff they must  contain atleast all the prime factors of k
     
     
 
@@ -25,25 +41,24 @@ For example, 5544 (2 * 2 * 2 * 3 * 3 * 7 * 11) is divisible by 84 (2 * 2 * 3 * 7
 class Solution {
 
     public long countPairs(int[] nums, int k) {
-        
-
         HashMap<Integer, Integer> gcdMap = new HashMap<>();
         long count = 0;
         for (int i = 0; i < nums.length; i++) {
-            long gcd_i =  gcd(nums[i] ,  k );
-            
-            for(var entry :  gcdMap.entrySet()){
-                if(gcd_i*entry.getKey()%k==0) {
-                	count+=entry.getValue();
+            long gcd_i = gcd(nums[i], k);
+
+            for (var entry : gcdMap.entrySet()) {
+                if (gcd_i * entry.getKey() % k == 0) {
+                    count += entry.getValue();
                 }
             }
-            gcdMap.put((int)gcd_i ,gcdMap.getOrDefault((int)gcd_i, 0)+1  );
+            gcdMap.put((int) gcd_i, gcdMap.getOrDefault((int) gcd_i, 0) + 1);
         }
 
         return count;
     }
-    private int gcd(int a, int b){
-        if(a==0) return b;
-        return gcd(b%a, a);
+
+    private int gcd(int a, int b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
     }
 }
