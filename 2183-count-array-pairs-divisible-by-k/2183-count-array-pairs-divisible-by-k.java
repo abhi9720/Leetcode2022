33a14cadc751b6b120630c98a16165e631835cdc
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-02 11:50:28 +0530

    Time: 1051 ms (26.76%), Space: 104.9 MB (60.51%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
@@ -1,3 +1,27 @@
+/*
+
+
+If (a*b)%k==0, then gcd(a,k) * gcd(b,k) % k is also 0.
+prove : 
+1. you can see this intuitively. When a number is divisible by another number, its prime factorization is a superset of the prime factorization of that number.
+
+For example, 5544 (2 * 2 * 2 * 3 * 3 * 7 * 11) is divisible by 84 (2 * 2 * 3 * 7).
+
+2. For example, GDC of 198 (2 * 3 * 3 * 11) and 84 (2 * 2 * 3 * 7) is 6 (2 * 3).
+    With that, we can see that 
+    
+    gcd(n1, k) * gcd(n2, k)% k = n1 * n2 % k. 
+    gcd =  maximum common factor 
+   
+    gcd(n1, k) - common between n1 and k
+    gcd(n2, k) - and other left will be provided by n2 and k
+    
+    n1 * n2 % k = which is equal to n1*n2 % k
+    
+    
+
+*/
+
 class Solution {
 
     public long countPairs(int[] nums, int k) {
@@ -7,7 +31,7 @@ class Solution {
         long count = 0;
         for (int i = 0; i < nums.length; i++) {
             long gcd_i =  gcd(nums[i] ,  k );
-            for(var entry :  gcdMap.entrySet()){
+            for(Map.Entry<Integer,Integer> entry :  gcdMap.entrySet()){
                 if(gcd_i*entry.getKey()%k==0) {
                 	count+=entry.getValue();
                 }
