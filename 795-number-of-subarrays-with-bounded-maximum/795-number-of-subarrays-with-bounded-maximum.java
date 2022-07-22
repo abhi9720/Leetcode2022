9c48721f6ce90295f4bd21f636785c1ca7b6d4ab
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-22 17:24:53 +0530

    Time: 8 ms (9.66%), Space: 71.7 MB (10.29%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
@@ -1,27 +1,23 @@
 class Solution {
+
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
         int c = 0;
         int count = 0;
+        int max = Integer.MIN_VALUE;
         int prev = 0;
-        
-        for(int ele : nums){            
-            
-            if(ele >= left && ele<=right){                
+        int i = 0;
+        for (int ele : nums) {
+            max = Math.max(max, ele);
+
+            if ( max <= right) {
                 c++;
-                count+=c;                
-                prev = c;                                
-            }
-            else if(ele < left){
-                count+=prev;
-                c++;
-            }
-            else{
-                c = 0;   
+                count += (ele < left) ? prev : c;
+                prev = ele < left ? prev : c;
+            } else {
+                c = 0;
                 prev = 0;
-            }                        
-            
-            
-            
+                max = Integer.MIN_VALUE;
+            }
         }
         return count;
     }
