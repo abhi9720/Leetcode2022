64beb89f5dde6952a18b34a7a4f8fb6001495e08
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-24 21:58:00 +0530

    Time: 3 ms (85.48%), Space: 42.8 MB (44.22%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n   =  s.length() , m =  p.length();
+        boolean dp[][] = new boolean[n+1][m+1];
+        
+        // i  for s
+        // j for  p
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 && j==0){
+                    dp[i][j] =  true;
+                }
+                else if(j==0){
+                    dp[i][j] =  false;
+                }
+                else if(i==0){
+                    char pc =  p.charAt(j-1);
+                    if(pc=='*'){
+                        dp[i][j] = dp[i][j-2];
+                    }
+                }
+                else{
+                    char pc = p.charAt(j-1);
+                    char sc =  s.charAt(i-1);
+                    if(pc==sc || pc=='.'){
+                        dp[i][j] =  dp[i-1][j-1];
+                    }
+                    else if(pc=='*'){
+                        dp[i][j] =  dp[i][j-2];
+                        int prev_pc =  p.charAt(j-2);
+                        if(prev_pc==sc || prev_pc=='.' ){
+                            dp[i][j] =  dp[i][j] || dp[i-1][j];
+                        }
+                    }                                         
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
