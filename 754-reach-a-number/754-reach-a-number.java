89e9281b9fca11879f33941adef48fe5d65c6a90
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-26 10:06:27 +0530

    Time: 2 ms (67.21%), Space: 41.6 MB (6.47%) - LeetHub

--- 754-reach-a-number/754-reach-a-number.java
+++ 754-reach-a-number/754-reach-a-number.java
@@ -1,13 +1,19 @@
 class Solution {
     public int reachNumber(int target) {
-        target =  Math.abs(target);        
-        int sum = 0;
-        for(int i=0;true;i++){
-             sum = getSum(i);            
-            if( target <= sum && ((sum-target)&1)==0 ){
-                return i;
-            }
+        // target =  Math.abs(target);        
+        // int sum = 0;
+        // for(int i=0;true;i++){
+        //      sum = getSum(i);            
+        //     if( target <= sum && ((sum-target)&1)==0 ){
+        //         return i;
+        //     }
+        // }
+        target =  Math.abs(target);
+        int k = 0;
+        while(target>0){
+            target-=++k;            
         }
+        return target%2==0?k:k+1+k%2;
         
     }
     private int getSum(int n ){
