e59f2915f59cc7e9f2c7b258d28a82bcdb0ef876
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-26 10:34:51 +0530

    Time: 12 ms (18.38%), Space: 48.2 MB (72.16%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,34 +1,53 @@
 class MinStack {
-    Stack<Integer> data , min;
+    private Stack<Long> stk;
+    private long minEle;
+
     public MinStack() {
-        data  =  new Stack<>();
-        min =  new Stack<>();
+        stk = new Stack<>();
+        minEle = Integer.MIN_VALUE;
     }
-    
+
     public void push(int val) {
-        data.push(val);
-		if(min.size()==0 || min.peek() >= val ){
-			min.push(val);
-		}
-        
+        Long l = new Long(val);
+        if (stk.size() == 0) {
+            stk.push(l);
+            minEle = val;
+        } else if (l < minEle) {
+            long newVal = l + (l - minEle);
+            stk.push(newVal);
+            minEle = val;
+        } else {
+            stk.push(l);
+        }
     }
-    
+
     public void pop() {
-        int popped =  data.pop();        
-		if( popped ==  min.peek()){            
-			min.pop();
-		}		
-        
+        if (stk.size() == 0); else if (stk.peek() < minEle) {
+            long ele = stk.pop();
+            minEle = minEle + (minEle - ele);
+        } else {
+            stk.pop();
+        }
     }
-    
+
     public int top() {
-		if(data.size()==0) return -1;
-        return data.peek();
+        if (stk.size() == 0) return -1; else {
+            long peek =  stk.peek();
+            if (peek < minEle) {
+                return (int)minEle;
+            } else return (int)peek;
+        }
     }
-    
+
     public int getMin() {
-        if(min.size() ==0 ) return -1;
-		return min.peek();
+        if (stk.size() == 0) return -1; else return (int) minEle;
     }
 }
-
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
