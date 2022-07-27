74ca1d20c89afad73961234da2dc3d842820e77b
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-28 00:25:52 +0530

    Time: 4 ms (87.22%), Space: 42.3 MB (81.11%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public String greatestLetter(String s) {
+        int freq[] = new int[26];
+        int MOD = 10001;
+        for (char ch : s.toCharArray()) {
+            int idx = ch > 'Z' ? ch - 'a' : ch - 'A';
+            // freq[idx] =  ch >'Z' ?  (freq[idx]/MOD +1)*MOD + freq[idx]%MOD +1
+            //     : (freq[idx]/MOD )*MOD + freq[idx]%MOD +1 ;
+            int small = freq[idx] % MOD;
+            int large = freq[idx] / MOD;
+            if (ch > 'Z') {                                
+                large =  large+1;                
+            }
+            else{
+                small =  small+1;
+            }
+            freq[idx] =  large*MOD + small;
+        }
+        
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] != 0) {
+                int small = freq[i] % MOD;
+                int large = freq[i] / MOD;
+                if (small > 0 && large > 0) {
+                    return (char) ('A' + i) + "";
+                }
+            }
+        }
+        return "";
+    }
+}
