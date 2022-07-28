399d07b9eab78f982306336747760a5e2dae9404
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-07-29 01:52:15 +0530

    Time: 5 ms (59.46%), Space: 54.8 MB (45.04%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    public int[][] intervalIntersection(int[][] l1, int[][] l2) {
+        Arrays.sort(l1, (o1, o2) -> o1[0] - o2[0]);
+        Arrays.sort(l2, (o1, o2) -> o1[0] - o2[0]);
+        int i = 0, j = 0;
+        int n1 = l1.length;
+        int n2 = l2.length;
+        List<int[]> ans = new ArrayList<>();
+        while (i < n1 && j < n2) {
+            int s1 = l1[i][0], e1 = l1[i][1];
+            int s2 = l2[j][0], e2 = l2[j][1];
+
+            if (s2 <= e1 && s2 >= s1) {
+                int s = Math.max(l1[i][0], l2[j][0]);
+                int e = Math.min(l1[i][1], l2[j][1]);
+                ans.add(new int[] { s, e });
+                // System.out.println("block first :  " + s + " : " + e);
+                if (e1 > e2) {
+                    j++;
+                    // System.out.println("j++");
+                } else {
+                    i++;
+                    // System.out.println("i++");
+                }
+            } else if (s1 <= e2 && s1 >= s2) {
+                // System.out.println(i+" "+j+"   true  : s1 <= e2 && s1 >= s2");
+                int s = Math.max(l1[i][0], l2[j][0]);
+                int e = Math.min(l1[i][1], l2[j][1]);
+                ans.add(new int[] { s, e });
+                // System.out.println("block second :  " + s + " : " + e);
+                if (e1 > e2) {
+                    j++;
+                    // System.out.println("j++");
+                } else {
+                    i++;
+                    // System.out.println("i++");
+                }
+            } else {
+                if (e1 > e2) {
+                    j++;
+                    // System.out.println("j++");
+                } else {
+                    i++;
+                    // System.out.println("i++");
+                }
+            }
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
