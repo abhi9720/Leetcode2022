9a45f4411c0f87b7cdfb3047568a9a15f4fa3547
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-08-10 22:44:09 +0530

    Time: 36 ms (41.36%), Space: 58.1 MB (14.03%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,20 +1,42 @@
 class Solution {
+
     public int coinChange(int[] coins, int amount) {
-        int amt[]  =  new int[amount+1];
-        // amt[i] = minimum number of coins require to make amount i
-        
-        for(int i=1;i<=amount;i++){
-            int minCoins = -1;
-            
-            // making ith amount using all coins             
-            for(int coin : coins ){
-                if( coin <= i  && amt[i-coin]!=-1 ){
-                    int req =  amt[i-coin]+1;
-                    minCoins =  minCoins<0?req:Math.min(minCoins,req);
+        //         int amt[]  =  new int[amount+1];
+        //         // amt[i] = minimum number of coins require to make amount i
+
+        //         for(int i=1;i<=amount;i++){
+        //             int minCoins = -1;
+
+        //             // making ith amount using all coins
+        //             for(int coin : coins ){
+        //                 if( coin <= i  && amt[i-coin]!=-1 ){
+        //                     int req =  amt[i-coin]+1;
+        //                     minCoins =  minCoins<0?req:Math.min(minCoins,req);
+        //                 }
+        //             }
+        //             amt[i] = minCoins;
+        //         }
+        //      return amt[amount];
+        int n = coins.length;
+        int dp[][] = new int[n + 1][amount + 1];
+        for (int j = 0; j <= amount; j++) {
+            dp[0][j] = 10001;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
-            amt[i] = minCoins;
         }
-     return amt[amount];
+        
+
+        return dp[n][amount] >= 10001 ? -1 : dp[n][amount];
     }
 }
