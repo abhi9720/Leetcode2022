bb402b5ff0e1a87aff11ed32669262f89232a994
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-08-20 10:43:51 +0530

    Time: 270 ms (31.60%), Space: 68 MB (41.51%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.java
@@ -0,0 +1,42 @@
+class Solution {
+    boolean dp[][] ;
+    public boolean checkPartitioning(String s) {
+        
+        int n = s.length();
+        dp =  new boolean[n][n];
+        precomputeAllpalindrome(s);
+        for (int i = 0; i < n - 1; i++) {
+            if (dp[0][i]) {
+                for (int j = i + 1; j < n - 1; j++) {
+                    if (dp[i+1][j] && dp[j+1][n-1]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void precomputeAllpalindrome(String str){
+        int n  =  str.length();
+        // gap stragety 
+        for(int i=0;i<n;i++){
+            dp[i][i] = true;
+        }
+        for(int gap =  0;gap<n;gap++){
+            for(int i =  0, j =  i+gap; j<n && i<n; i++,j++){
+                if(gap==0){
+                    dp[i][j] =  true;
+                }
+                else if(gap==1){
+                    dp[i][j] =  str.charAt(i) == str.charAt(j);
+                }
+                else{
+                    dp[i][j] = ( str.charAt(i)== str.charAt(j)) && dp[i+1][j-1];
+                }                    
+            }                        
+        }        
+        
+    }
+   
+}
