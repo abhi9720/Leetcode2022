3c9ccc594cd341fe6018563a4a29d254129dac05
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-08-25 10:39:50 +0530

    Time: 32 ms (27.52%), Space: 76.8 MB (17.95%) - LeetHub

--- 491-increasing-subsequences/491-increasing-subsequences.java
+++ 491-increasing-subsequences/491-increasing-subsequences.java
@@ -1,14 +1,14 @@
 class Solution {
 
     public List<List<Integer>> findSubsequences(int[] nums) {
-        ArrayList<Integer> seq = new ArrayList<>();
+        LinkedList<Integer> seq = new LinkedList<>();
         Set<List<Integer>> ans = new HashSet<>();
         helper(nums, ans, seq, 0, Integer.MIN_VALUE);
-        List<List<Integer>> fans = new ArrayList<>(ans);
+        List<List<Integer>> fans = new LinkedList<>(ans);
         return fans;
     }
 
-    private void helper(int nums[], Set<List<Integer>> ans, ArrayList<Integer> seq, int idx, int next) {
+    private void helper(int nums[], Set<List<Integer>> ans,LinkedList<Integer> seq, int idx, int next) {
         if (idx == nums.length) {
             if (seq.size() > 1) ans.add(new ArrayList<>(seq));
             return;
@@ -21,7 +21,7 @@ class Solution {
         if (nums[idx] >= next) {
             seq.add(nums[idx]);
             helper(nums, ans, seq, idx + 1, nums[idx]);
-            seq.remove(seq.size() - 1);
+            seq.removeLast();
         }
     }
 }
