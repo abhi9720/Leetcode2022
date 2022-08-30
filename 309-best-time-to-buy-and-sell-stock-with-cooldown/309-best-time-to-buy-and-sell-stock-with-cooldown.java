4e2f3c9548c4d03adf0b2ead5d31ea7fac674de8
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-08-30 23:39:40 +0530

    Time: 1 ms (94.39%), Space: 41.8 MB (82.30%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,8 +2,17 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n  =  prices.length;
         int b1 =  -prices[0] , s1 = 0 , c1 = 0; 
+        /*
+        buy[i]=  max( previous buy , buy after cooldown)
+        sell[i] =  max( previous_sell , price[i] - previous_buy  )
+        cooldown[i] =  maximum profit of any sequence if day i is cooldown
+        which will be max of previous_cooldown , previous_sell
+        
+        */
         for(int i=1;i<n;i++){
+            
             int b2 = Math.max(-prices[i]+c1 , b1);
+            
             int s2 = Math.max( b1+prices[i] , s1 );
             int c2 =  Math.max(c1,s1);
             c1 =  c2;
