15b3ffa3cc521c79dec662a0def06db919e4a9b1
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-08-30 22:16:15 +0530

    Time: 11 ms (48.15%), Space: 71 MB (65.65%) - LeetHub

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n  =  prices.length;
+        int buy[] =  new int[n];
+        int sell[]  =  new int[n];
+        // if we buy something that means we have spend that amount of money
+        buy[0] =  -prices[0];
+        sell[0] =  0;
+        for(int i=1;i<n;i++){
+            buy[i] = Math.max(buy[i-1] , sell[i-1] - prices[i]);
+            sell[i] =  Math.max( buy[i-1] +prices[i]-fee, sell[i-1]     );
+        }
+        
+        return Math.max(buy[n-1] ,sell[n-1]);
+        
+        
+    }
+}
