9dc411c550d6491b8790e0b6dc087294cc210491
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-09-04 23:57:35 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (60.66%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -14,7 +14,17 @@ class Solution {
             //     }
             //     dp[i][j] = max;
             // }
-            int maxP =  Integer.MIN_VALUE;
+            /*
+            example => dp[6][4]  ->
+                                dp[5][4] + 0
+                                dp[5][3] - price[3] + price[6]
+                                dp[5][2] -  price[2] + preice[6]
+                                ans so on ..............    
+                                we can genralize it 
+                initialize it -> maxp =  Integer.MIN_VALUE ;                                                           maxP =  Math.max(maxP , dp[i-1][j-1] -  prices[j-1]);                            
+                
+            */
+            int maxP =  Integer.MIN_VALUE;            
             for(int j =  1;j<n;j++){
                 maxP =  Math.max(maxP , dp[i-1][j-1] -  prices[j-1]);
                 dp[i][j] =  Math.max(maxP + prices[j] , dp[i][j-1] );
