a70b27277c8b7757143a4f4b364f822f699e1fe1
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-09-05 21:19:37 +0530

    Time: 1 ms (95.21%), Space: 42.1 MB (58.91%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -5,13 +5,16 @@ class Solution {
         int j =  n2-1;
         int c1 = 0,c2 =0;
         while( i>=0 || j>=0 ){
-            c1 = 0;
             
+            //backspace count 
+            c1 = 0;            
             for(;i>=0;i--){                
                 if(s.charAt(i)=='#' ){ c1++;}
-                else if( c1>0 ){c1--;}   
+                else if( c1>0 ){c1--;}   // do backspace
                 else break;
             }
+            
+            //backspace count 
             c2 = 0;
             for(;j>=0;j--){                
                 if(t.charAt(j)=='#' ){ c2++;}
@@ -20,13 +23,15 @@ class Solution {
             }
             
             
-            
-            if( i>=0 && j>=0 && s.charAt(i) == t.charAt(j) ) {i--;j--;}
+
+            if( i>=0 && j>=0 && s.charAt(i) == t.charAt(j) ) {
+                i--;
+                j--;
+            }
             else break;
             
             
         }
         return i == -1 && j == -1;
-        
     }
 }
