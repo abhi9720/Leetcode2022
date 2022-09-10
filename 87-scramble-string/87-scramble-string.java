0bb269ae1f47e3e0d497953ec628d9dc4bf49deb
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-09-10 12:57:32 +0530

    Time: 27 ms (51.80%), Space: 46.7 MB (59.09%) - LeetHub

--- 87-scramble-string/87-scramble-string.java
+++ 87-scramble-string/87-scramble-string.java
@@ -1,59 +1,38 @@
 class Solution {
-    Map<String, Boolean> map;
+    HashMap<String, Boolean> dp;
 
     public boolean isScramble(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
-        map = new HashMap<>();
+        dp = new HashMap<>();
         return helper(s1, s2);
     }
 
     private boolean helper(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
-        if (s1.length() == 1) {
-            return s1.equals(s2);
-        }
+        int n1 = s1.length(), n2 = s2.length();
+        if (n1 != n2) return false;
+        if (s1.equals(s2)) return true;
+        String key = s1 + "," + s2;
+        if (dp.containsKey(key)) return dp.get(key);
+
         char ch1[] = s1.toCharArray();
         char ch2[] = s2.toCharArray();
+
         Arrays.sort(ch1);
         Arrays.sort(ch2);
-        String temp1 = new String(ch1);
-        String temp2 = new String(ch2);
+        if (!(new String(ch1).equals(new String(ch2)))) return false;
+        for (int i = 1; i < n1; i++) {
+            // 1st with 1st and 2nd with second
+            boolean ans1 = helper(s1.substring(0, i), s2.substring(0, i)) 
+                && helper(s1.substring(i), s2.substring(i));
+            // 1st with 2nd part and 2nd part with 1st part
+            boolean ans2 = helper(s1.substring(0, i), s2.substring(n1 - i)) && helper(s1.substring(i), s2.substring(0, n1 - i));
 
-        if (!temp1.equals(temp2)) {
-            return false;
-        }
-
-        String key = s1 + "," + s2;
-        if (map.containsKey(key)) {
-            return map.get(key);
-        }
-
-        int n = s1.length();
-        // split in mcm pattern
-        for (int i = 1; i < n; i++) {
-            // Check if S2[0...i] is a scrambled
-            // string of S1[0...i] and if S2[i+1...n]
-            // is a scrambled string of S1[i+1...n]
-            if (helper(s1.substring(0, i), s2.substring(0, i)) && helper(s1.substring(i, n), s2.substring(i, n))) {
-                map.put(key, true);
-                return true;
-            }
-
-            // Check if S2[0...i] is a scrambled
-            // string of S1[n-i...n] and S2[i+1...n]
-            // is a scramble string of S1[0...n-i-1]
-            if (helper(s1.substring(n - i, n), s2.substring(0, i)) && helper(s1.substring(0, n - i), s2.substring(i, n))) {
-                map.put(key, true);
+            if (ans1 || ans2) {
+                dp.put(key, true);
                 return true;
             }
         }
-        map.put(key, false);
+
+        dp.put(key, false);
         return false;
     }
 }
-/*
-
-Apporach :
-1. Split the string in possible length 
-2. Check both path recursive 
-*/
