e474fd45e998d06f7602e928905571f86248f794
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-11-02 01:12:14 +0530

    Time: 2 ms (97.57%), Space: 45.7 MB (76.31%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] != '.') {
+                    if (!canPlace(board, i, j, board[i][j])) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean canPlace(char[][] board, int i, int j, char toplace) {
+        int x1 = i - i % 3;
+        int y1 = j - j % 3;
+
+        // check submatrix 
+        for (int x = x1; x <= x1 + 2; x++) {
+            for (int y = y1; y <= y1 + 2; y++) {
+                if (x != i && y != j) if (board[x][y] == toplace) return false;
+            }
+        }
+
+        // check column
+        for (int x = 0; x < board.length; x++) {
+            if (x != i) if (board[x][j] == toplace) return false;
+        }
+
+        // check row 
+        for (int y = 0; y < board.length; y++) {
+            if (y != j) if (board[i][y] == toplace) return false;
+        }
+        
+        return true;
+    }
+}
