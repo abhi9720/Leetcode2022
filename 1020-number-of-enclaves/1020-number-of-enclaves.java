3ba5940475e9b15112f44959a98298e06acde567
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-11-20 05:06:00 +0530

    Time: 26 ms (16.67%), Space: 75.9 MB (35.99%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,41 +1,33 @@
 class Solution {
-    int ans,res;
+    int ans, res;
+
     public int numEnclaves(int[][] grid) {
-        int cnt=0;
-        
-        for(int i=0;i<grid.length;i++)
-        {
-            
-            for(int j=0;j<grid[0].length;j++)
-            {
-                ans=0;
-                res=1;
-               if(grid[i][j]==1)
-              {
-                  
-               dfs(grid,i,j,grid.length,grid[0].length);
-                   if(res==0)
-                       cnt+=0;
-                   else cnt+=ans;
-                  System.out.println(res);
-              }
+        int cnt = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                ans = 0;
+                res = 1;
+                if (grid[i][j] == 1) {
+                    dfs(grid, i, j, grid.length, grid[0].length);
+                    if (res == 0) cnt += 0; else cnt += ans;
+                 
+                }
             }
         }
         return cnt;
     }
-    void dfs(int[][] grid,int row,int col,int m,int n)
-    {
-        if(row>=m||row<0||col>=n||col<0 || grid[row][col]==0) return;
-        if((row==m-1 || row==0||col==n-1||col==0) && grid[row][col]==1) {
-            res=0;
+
+    void dfs(int[][] grid, int row, int col, int m, int n) {
+        if (row >= m || row < 0 || col >= n || col < 0 || grid[row][col] == 0) return;
+        if ((row == m - 1 || row == 0 || col == n - 1 || col == 0) && grid[row][col] == 1) {
+            res = 0;
         }
-        ans+=1;
-        grid[row][col]=0;
-        dfs(grid,row+1,col,m,n);
-        dfs(grid,row,col+1,m,n);
-        dfs(grid,row,col-1,m,n);
-        dfs(grid,row-1,col,m,n);
-        
+        ans += 1;
+        grid[row][col] = 0;
+        dfs(grid, row + 1, col, m, n);
+        dfs(grid, row, col + 1, m, n);
+        dfs(grid, row, col - 1, m, n);
+        dfs(grid, row - 1, col, m, n);
     }
-    
 }
