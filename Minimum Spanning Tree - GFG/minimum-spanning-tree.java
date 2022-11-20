a9286185f6e25461c3ced4807f8d14c3fe6172f3
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-11-20 08:43:58 +0530

    Added solution - LeetHub

---
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int V = Integer.parseInt(str[0]);
+            int E = Integer.parseInt(str[1]);
+    
+            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
+            for(int i=0;i<V;i++)
+            {
+                adj.add(new ArrayList<ArrayList<Integer>>());
+            }
+            
+            int i=0;
+            while (i++<E) {
+                String S[] = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                int w = Integer.parseInt(S[2]);
+                ArrayList<Integer> t1 = new ArrayList<Integer>();
+                ArrayList<Integer> t2 = new ArrayList<Integer>();
+                t1.add(v);
+                t1.add(w);
+                t2.add(u);
+                t2.add(w);
+                adj.get(u).add(t1);
+                adj.get(v).add(t2);
+            }
+            
+            Solution ob = new Solution();
+            
+            System.out.println(ob.spanningTree(V, adj));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution
+{
+    //Function to find sum of weights of edges of the Minimum Spanning Tree.
+    static int spanningTree(int n, ArrayList<ArrayList<ArrayList<Integer>>> adj) 
+    {
+        boolean visited[] =  new boolean[n];
+        PriorityQueue<int[]> pq =  new PriorityQueue<>( (o1,o2) -> o1[1] - o2[1] );
+        pq.offer(new int[]{0,0});
+        int cost[] =  new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        
+        while(pq.size()!=0 ){
+            
+            int rm[] =  pq.remove();
+            if(visited[rm[0]]) continue;
+            
+            visited[rm[0]] =  true;
+            cost[rm[0]] =  Math.min(rm[1], cost[rm[0]]);
+            for(ArrayList<Integer> nbr : adj.get(rm[0]) ){
+                int nbrNode =  nbr.get(0) ;
+                if(!visited[nbrNode ] || cost[nbrNode] > nbr.get(1)  ){
+                    pq.offer(new int[]{nbrNode , nbr.get(1) });    
+                }
+            }
+        }
+        
+        int totalCost = 0;
+        for(int i=0;i<n;i++){
+            totalCost+=cost[i];
+        }
+        return totalCost;
+        
+    }
+}
