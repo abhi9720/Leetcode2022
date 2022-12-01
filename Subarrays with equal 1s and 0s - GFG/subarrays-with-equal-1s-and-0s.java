31190216b7dca2d63092f846636884becd577df7
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-12-01 20:15:55 +0530

    Added solution - LeetHub

--- Subarrays with equal 1s and 0s - GFG/subarrays-with-equal-1s-and-0s.java
+++ Subarrays with equal 1s and 0s - GFG/subarrays-with-equal-1s-and-0s.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial Template for Java
 
 import java.util.*;
@@ -23,34 +23,49 @@ class Driverclass
 	 }
 }
 
+
 // } Driver Code Ends
+
+
+//User function Template for Java
 
 
 //User function Template for Java
 
 class Solution
 {
-    //Function to count subarrays with 1s and 0s.
-    static int countSubarrWithEqualZeroAndOne(int arr[], int n)
-    {
-        // treat 0 as -1 ans 1 as 1 
+    // Function to count subarrays with 1s and 0s.
+    static int countSubarrWithEqualZeroAndOne(int arr[], int n){
+        
+        // treat 0 as -1 and 1 as 1 
         Map<Integer,Integer> map =  new HashMap<>();
         map.put(0,1);
-        int sum = 0,count = 0;
-        for(int ele : arr ){
-            sum+=(ele==0?-1:1);
-            if(map.containsKey(sum) ){
-                count+=map.get(sum);
-                map.put(sum,map.get(sum)+1);
+        int ans = 0,c0 = 0 , c1 = 0;
+        
+        // for(int ele : arr ){
+        //     sum+=(ele==0?-1:1);
+        //     if(map.containsKey(sum) ){
+        //         count+=map.get(sum);
+        //         map.put(sum,map.get(sum)+1);
+        //     } else {
+        //         map.put(sum,1);
+        //     }
+        // }
+        for(int ele : arr){
+            if(ele==0) c0++;
+            else c1++;
+            int diff =  c0-c1;
+            
+            if(map.containsKey(diff) ){
+                ans+= map.get(diff);
+                map.put(diff , map.get(diff)+1);
             }
             else{
-                map.put(sum,1);
+                map.put(diff,1);
             }
-            
         }
-        return count;
+        
+        return ans;
         
     }
 }
-
-
