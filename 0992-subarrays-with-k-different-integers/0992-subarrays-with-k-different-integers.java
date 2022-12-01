c54eab171cbfb31876f0ca860ee84a59036d9fda
Abhishek Tiwari <68281476+abhi9720@users.noreply.github.com>
2022-12-01 11:53:48 +0530

    Time: 82 ms (64.74%), Space: 74.6 MB (9.87%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -4,18 +4,22 @@ class Solution {
         return atMostK(A, K) - atMostK(A, K - 1);
     }
 
-    int atMostK(int[] A, int K) {
+    int atMostK(int[] A, int k) {
         int i = 0, res = 0;
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int j = 0; j < A.length; ++j) {
-            if (count.getOrDefault(A[j], 0) == 0) K--;
-            count.put(A[j], count.getOrDefault(A[j], 0) + 1);
-            while (K < 0) {
-                count.put(A[i], count.get(A[i]) - 1);
-                if (count.get(A[i]) == 0) K++;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int j = 0; j < A.length; j++) {
+            if (!map.containsKey(A[j])) k--;
+            map.put(A[j], map.getOrDefault(A[j], 0) + 1);
+
+            while (k < 0) {
+                int prev = map.put(A[i], map.get(A[i]) - 1);
+                if (prev == 1) {
+                    k++;
+                    map.remove(A[i]);
+                }
                 i++;
             }
-            res += j - i + 1;
+            res+= j-i+1;
         }
         return res;
     }
